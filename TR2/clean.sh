eabc4dc39d81a8cff8c973cc61838e77e71e3857
legluondunet <legluondunet@gmail.com>
2019-05-08 19:53:00 +0200

    Update clean.sh

--- TR2/clean.sh
+++ TR2/clean.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#wineconsole ogg2mp3.bat
+wineconsole ogg2mp3.bat
 for file in Track*.mp3
 do
 	mv -v "$file" "${file/Track/}"
@@ -8,5 +8,4 @@ cd ..
 mkdir tr2mainreg
 mv *.reg tr2mainreg
 rm -f -r __support/ gog* webcache.zip music/*.ogg music/ffmpeg.exe music/ogg2mp3.bat audio/
-mv music audio
 exit
