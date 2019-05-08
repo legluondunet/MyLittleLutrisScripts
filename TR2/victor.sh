755e20fb21ee2226b30a6cd59c85d7ac5118d187
legluondunet <legluondunet@free.fr>
2019-05-08 14:49:34 +0200

    nouveau fichier : TR2/victor.sh

---
+++ TR2/victor.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+wineconsole ogg2mp3.bat;
+for file in Track*.mp3; 
+do mv -v "$file" "${file/Track/}"; 
+done; 
+cd ..; 
+mkdir tr2mainreg;
+mv *.reg tr2mainreg;
+rm -f -r __support/ gog* webcache.zip music/*.ogg music/ffmpeg.exe music/ogg2mp3.bat; 
+
