28b299bc9a58c714cd765d9b4364ac0d10b622d0
legluondunet <legluondunet@free.fr>
2019-05-08 16:59:38 +0200

    modifié :         TR2/ogg2mp3.bat 	modifié :         TR2/victor.sh

--- TR2/ogg2mp3.bat
+++ TR2/ogg2mp3.bat
@@ -1,1 +1,1 @@
-for %%a IN (*.ogg) DO (ffmpeg -i "%%a" "%%~na.mp3")
+for %%a IN (*.ogg) DO (ffmpeg -i -loglevel panic -hide_banner "%%a" "%%~na.mp3")
--- TR2/victor.sh
+++ TR2/victor.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
-wineconsole ogg2mp3.bat;
-for file in Track*.mp3; 
-do mv -v "$file" "${file/Track/}"; 
-done; 
-cd ..; 
-mkdir tr2mainreg;
-mv *.reg tr2mainreg;
-rm -f -r __support/ gog* webcache.zip music/*.ogg music/ffmpeg.exe music/ogg2mp3.bat; 
-
+wineconsole ogg2mp3.bat
+for file in Track*.mp3 
+do mv -v "$file" "${file/Track/}"
+done
+cd .. 
+mkdir tr2mainreg
+mv *.reg tr2mainreg
+rm -f -r __support/ gog* webcache.zip music/*.ogg music/ffmpeg.exe music/ogg2mp3.bat audio/
+mv music audio;
+rm -f -r music/victor.sh
+exit
