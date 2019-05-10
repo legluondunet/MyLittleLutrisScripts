c474c49e8a8a45ec8eb19240e0a62bab57b962a2
legluondunet <legluondunet@free.fr>
2019-05-10 02:02:37 +0200

    modifié :         Indiana Jones and the Emperor's Tomb/IJETM2.BAT 	supprimé :        Indiana Jones and the Emperor's Tomb/gamepad profiles/.~lock.ijet_gamepad_readme.html# 	modifié :         Indiana Jones and the Emperor's Tomb/ijet.bmp 	nouveau fichier : Indiana Jones and the Emperor's Tomb/ijetniceconfig.bat 	supprimé :        Indiana Jones and the Emperor's Tomb/ijetniceconfig.sh

--- Indiana Jones and the Emperor's Tomb/ijetniceconfig.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-cd "drive_c/GOG Games/Indiana Jones and the Emperor's Tomb/GameData/indy/"
-sed -i -e 's/\(^res_width=\).*/\11280/' -e 's/\(^res_height=\).*/\1960/' -e 's/\(^res_bpp=\).*/\132/' -e 's/\(^fpsLimit=\).*/\160/' "vars.cfg"
