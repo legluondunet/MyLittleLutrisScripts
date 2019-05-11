cf6b97ee45851ca17498684d4d88d0fd61a8b6cd
legluondunet <legluondunet@gmail.com>
2019-05-11 22:06:25 +0200

    Update ogg2mp3.bat

--- Tomb Raider 2/ogg2mp3.bat
+++ Tomb Raider 2/ogg2mp3.bat
@@ -8,6 +8,7 @@ echo ---------------------------------------------------------
 echo.
 echo.
 echo.
+cd c:\GOG Games\Tomb Raider 2\music\
 for %%a IN (*.ogg) DO (
 	echo %%a & ffmpeg -v warning -hide_banner -stats -i "%%a" "%%~na.mp3"
 )
