3abf9d666ceeb45e254e28b9da4e13194263a624
legluondunet <legluondunet@gmail.com>
2019-05-11 08:44:00 +0200

    Update script.bat

--- Heretic II/script.bat
+++ Heretic II/script.bat
@@ -1,14 +1,5 @@
-echo off
 cd c:\tmp
-pause
 move "heretic ii\ht2_ep_v1-07.7z" .
-pause
-zip -r -0 hereticiioldinstall.zip "c:\program files (x86)\heretic ii"
-pause
 move "c:\program files (x86)\heretic ii" "c:\tmp\hereticiioldinstall"
-pause
-unzip -LL hereticii.zip -d ../
-pause
-zip -r -0 "heretic ii.zip" "heretic ii"
-pause
+unzip -LL hereticiioldinstall.zip -d ../
 unzip -LL -o "heretic ii.zip" -d "c:\program files (x86)"
