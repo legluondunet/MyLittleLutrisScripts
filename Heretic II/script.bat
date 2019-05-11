08a6fc0f5e9938a7f4add36f5bd70a584f751eb6
legluondunet <legluondunet@free.fr>
2019-05-11 07:47:09 +0200

    nouveau fichier : Heretic II/Heretic II - Lutris script.txt 	nouveau fichier : Heretic II/script.bat 	modifié :         Indiana Jones and the Emperor's Tomb/gamepad profiles/ijet_gamepad_readme.html

---
+++ Heretic II/script.bat
@@ -0,0 +1,8 @@
+echo off
+cd c:\tmp
+move "heretic ii\ht2_ep_v1-07.7z" .
+zip -r -0 hereticiioldinstall.zip "c:\program files (x86)\heretic ii"
+move "c:\program files (x86)\heretic ii" "c:\tmp\hereticiioldinstall"
+unzip -LL hereticii.zip -d ../
+zip -r -0 "heretic ii.zip" "heretic ii"
+unzip -LL -o patch.zip -d "c:\program files (x86)"
