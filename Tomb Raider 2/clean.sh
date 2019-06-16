4d985bb0c190a065f0b80e1b0bbe8f59e38a30c2
legluondunet <legluondunet@free.fr>
2019-06-17 00:17:00 +0200

    supprimé :        Tomb Raider 2/TR2Main-20180105.zip 	renommé :         Tomb Raider 2/Tomb Raider 2 GOG - Lutris Script.txt -> Tomb Raider 2/Tomb Raider 2 - GOG.txt 	modifié :         Tomb Raider 2/clean.sh

--- Tomb Raider 2/clean.sh
+++ Tomb Raider 2/clean.sh
@@ -4,8 +4,6 @@ do
 	mv -v "$file" "${file/Track/}"
 done
 cd .. 
-mkdir tr2mainreg
-mv *.reg tr2mainreg
 rm -f -r __support/ gog* webcache.zip music/*.ogg music/ffmpeg.exe music/ogg2mp3.bat audio/
 mv music audio
 exit
