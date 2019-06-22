d46e7be699fb3bbde11f71341e83379a582559ff
legluondunet <legluondunet@free.fr>
2019-06-22 21:49:00 +0200

    modifié :         Heretic II/h2local.sh

--- Heretic II/h2local.sh
+++ Heretic II/h2local.sh
@@ -4,8 +4,8 @@ lang=$1
 echo $lang
 cd HERETICIICDL/base
 rm -f -r spanish-1.pak
-if [ $lang = "fr" ]; then tar xfv ../TMP/french-1.pak.tar.xz
-elif [ $lang = "ge" ]; then tar xfv ../TMP/german-1.pak.tar.xz
-elif [ $lang = "it" ]; then tar xfv ../TMP/italian-1.pak.tar.xz
-elif [ $lang = "sp" ]; then tar xfv ../TMP/spanish-1.pak.tar.xz
+if [ $lang = "fr" ]; then tar xfv ../TMP/data/french-1.pak.tar.xz
+elif [ $lang = "ge" ]; then tar xfv ../TMP/data/german-1.pak.tar.xz
+elif [ $lang = "it" ]; then tar xfv ../TMP/data/italian-1.pak.tar.xz
+elif [ $lang = "sp" ]; then tar xfv ../TMP/data/spanish-1.pak.tar.xz
 fi
