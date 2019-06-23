c0162f126bc9b7c5bde4f8f8074bc8db12b2728a
legluondunet <legluondunet@free.fr>
2019-06-23 15:36:22 +0200

    modifié :         Heretic II/defconfig.sh 	modifié :         Heretic II/gamepad profiles/h2_x360.gamecontroller.amgp

--- Heretic II/defconfig.sh
+++ Heretic II/defconfig.sh
@@ -22,16 +22,19 @@ sed -i 's/set gl_modulate.*/set gl_modulate "3"/' default.cfg
 sed -i 's/set gl_particle_att_c.*/set gl_particle_att_c "0.01"/' default.cfg
 sed -i 's/set gl_particle_att_b.*/set gl_particle_att_b "0.01"/' default.cfg
 sed -i 's/set fov.*/set fov "90"/' default.cfg
-sed -i 's/s_volume.*/s_volume "0.5"/' default.cfg
+sed -i 's/set s_volume.*/s_volume "0.5"/' default.cfg
 sed -i 's/set gl_sortmulti.*/set gl_sortmulti "1"/' default.cfg
 sed -i 's/set autoweapon.*/set autoweapon "0"/' default.cfg
-sed -i 'set set cl_run.*/set cl_run "0"/' default.cfg
-sed -i 'set set cl_doautoaim.*/set cl_doautoaim "0"/' default.cfg
+sed -i 's/set cl_run.*/set cl_run "1"/' default.cfg
+sed -i 's/set cl_doautoaim.*/set cl_doautoaim "0"/' default.cfg
+sed -i 's/bind ` "toggleconsole"/bind Tab "toggleconsole" toggleconsole"/' default.cfg
+sed -i 's/bind Tab "+lookaround"/bind "+lookaround"/' default.cfg
 
 
 sed -i '$ a set con_alpha "0.5"' default.cfg
 sed -i '$ a set win_noalttab "0"' default.cfg
 sed -i '$ a set r_detail "3"' default.cfg
 sed -i '$ a set blood_level "3"' default.cfg
-sed -i '$ a set set in_joystick "0"' default.cfg
+sed -i '$ a set in_joystick "0"' default.cfg
+sed -i '$ a set set autoweapon "0"' default.cfg
 
