efdad82379cfde95b30a552f1a070bfde0b3ae2d
legluondunet <legluondunet@free.fr>
2019-06-25 00:02:56 +0200

    modifié :         Heretic II/h2local.sh

--- Heretic II/h2local.sh
+++ Heretic II/h2local.sh
@@ -2,15 +2,19 @@
 
 lang=$1
 echo $lang
+
 if [ -f "HERETICIICDL/heretic2" ]; then 
 dir=HERETICIICDL/base
 cd HERETICIICDL/TMP/data
 fi
+
 if [ -f "../Program Files (x86)/Heretic II/" ]; then
 dir="../Program Files (x86)/Heretic II/base"
 cd data
 fi
-rm -f -r french-1.pak.tar.xz german-1.pak.tar.xz italian-1.pak.tar.xz spanish-1.pak
+
+rm -f -r french-1.pak german-1.pak italian-1.pak spanish-1.pak
+
 if [ $lang = "fr" ]; then tar xfv french-1.pak.tar.xz -C "$dir"
 elif [ $lang = "ge" ]; then tar xfv german-1.pak.tar.xz -C "$dir"
 elif [ $lang = "it" ]; then tar xfv italian-1.pak.tar.xz -C "$dir"
