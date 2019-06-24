df6f54726656e98bded39634aba54790b46fc842
legluondunet <legluondunet@free.fr>
2019-06-25 00:23:15 +0200

    modifié :         Heretic II/h2local.sh

--- Heretic II/h2local.sh
+++ Heretic II/h2local.sh
@@ -4,16 +4,21 @@ lang=$1
 echo $lang
 
 if [ -f "HERETICIICDL/heretic2" ]; then 
-dir=HERETICIICDL/base
-cd HERETICIICDL/TMP/data
+dir=../../base
+cd HERETICIICDL/base
+rm -f -r french-1.pak german-1.pak italian-1.pak spanish-1.pak
+cd ../TMP/data
 fi
 
 if [ -f "../Program Files (x86)/Heretic II/" ]; then
-dir="../Program Files (x86)/Heretic II/base"
-cd data
+dir="../../Program Files (x86)/Heretic II/base"
+cd "../Program Files (x86)/Heretic II/base"
+rm -f -r french-1.pak german-1.pak italian-1.pak spanish-1.pak
+cd ../../../tmp/data
 fi
 
-rm -f -r french-1.pak german-1.pak italian-1.pak spanish-1.pak
+echo "The script you are running has basename `basename "$0"`, dirname `dirname "$0"`"
+echo "The present working directory is `pwd`"
 
 if [ $lang = "fr" ]; then tar xfv french-1.pak.tar.xz -C "$dir"
 elif [ $lang = "ge" ]; then tar xfv german-1.pak.tar.xz -C "$dir"
