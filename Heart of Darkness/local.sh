cd9fcacf487fb0eb21cb0ef446d813e8e959a103
legluondunet <legluondunet@gmail.com>
2019-06-29 08:25:12 +0200

    Update local.sh

--- Heart of Darkness/local.sh
+++ Heart of Darkness/local.sh
@@ -5,5 +5,5 @@ cp HeartOfDarkness_Fr.EXE HeartOfDarkness.EXE
 elif [ -f HeartOfDarkness_Ge.EXE ]; then 
 cp HeartOfDarkness_Ge.EXE HeartOfDarkness.EXE
 elif [ -f HeartOfDarkness_En.EXE ]; then 
-cpHeartOfDarkness_En.EXE HeartOfDarkness.EXE
+cp HeartOfDarkness_En.EXE HeartOfDarkness.EXE
 fi
