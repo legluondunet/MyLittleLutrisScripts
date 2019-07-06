4ef891e643f01e3b358afc5b5cec3afe7d9e3c2f
legluondunet <legluondunet@free.fr>
2019-07-06 14:45:24 +0200

    nouveau fichier : Unreal/defconfig.sh 	modifié :         Unreal/unreal_patch_227i_MULTI5.zip 	modifié :         X-Men Origins - Wolverine/X-Men Origins - Wolverine - CD.txt

---
+++ Unreal/defconfig.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+lang=$1
+width=$2
+height=$3
+
+echo $lang $width $height
+cd "drive_c/GOG Games/Unreal Gold/System/"
+
+cp Default.ini Default.ini.ori
+sed -i 's/GameRenderDevice=.*/GameRenderDevice=OpenGLDrv.OpenGLRenderDevice/' Default.ini
+sed -i 's/Language=.*/Language='$lang'/' Default.ini
+sed -i 's/FirstRun=.*/FirstRun=227/' Default.ini
+sed -i 's/FullscreenViewportX=.*/FullscreenViewportX='$width'/' Default.ini
+sed -i 's/FullscreenViewportX=.*/FullscreenViewporty='$height'/' Default.ini
+
+
+cp Unreal.ini Unreal.ini.ori
+sed -i 's/GameRenderDevice=.*/GameRenderDevice=OpenGLDrv.OpenGLRenderDevice/' Unreal.ini
+sed -i 's/Language=.*/Language='$lang'/' Unreal.ini
+sed -i 's/FirstRun=.*/FirstRun=227/' Unreal.ini
+sed -i 's/FullscreenViewportX=.*/FullscreenViewportX='$width'/' Unreal.ini
+sed -i 's/FullscreenViewportX=.*/FullscreenViewporty='$height'/' Unreal.ini
+
+
+cp DefaultLinux.ini DefaultLinux.ini.ori
+sed -i 's/Language=.*/Language='$lang'/' DefaultLinux.ini
+sed -i 's/FullscreenViewportX=.*/FullscreenViewportX='$width'/' DefaultLinux.ini
+sed -i 's/FullscreenViewportY=.*/FullscreenViewportY='$height'/' DefaultLinux.ini
+
+cp UnrealLinux.ini UnrealLinux.ini.ori
+sed -i 's/Language=.*/Language='$lang'/' UnrealLinux.ini
+sed -i 's/FullscreenViewportX=.*/FullscreenViewportX='$width'/' UnrealLinux.ini
+sed -i 's/FullscreenViewportY=.*/FullscreenViewportY='$height'/' UnrealLinux.ini
