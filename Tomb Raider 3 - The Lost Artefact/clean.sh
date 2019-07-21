ee7e47c468a57005de413b1e34371ade59f64f12
legluondunet <legluondunet@free.fr>
2019-07-22 00:20:58 +0200

    modifié :         Tomb Raider 1/Tomb Raider 1 - GOG.txt 	modifié :         Tomb Raider 2/Tomb Raider II - GOG.txt 	renommé :         Tomb Raider 3 - Adventures of Lara Croft/Tomb Raider 3 - GOG.txt -> Tomb Raider 3 - Adventures of Lara Croft/Tomb Raider 3 - Adventures of Lara Croft - GOG.txt 	modifié :         Tomb Raider 3 - The Lost Artefact/TR3TLAM2.BAT 	modifié :         Tomb Raider 3 - The Lost Artefact/Tomb Raider - The Last Artefact - CD.txt 	supprimé :        Tomb Raider 3 - The Lost Artefact/clean.sh 	nouveau fichier : Tomb Raider 4 - The Last Revelation/TR4TLRM1.BAT 	nouveau fichier : Tomb Raider 4 - The Last Revelation/TR4TLRM2.BAT 	nouveau fichier : Tomb Raider 4 - The Last Revelation/Tomb Raider 4 - The Last Revelation - GOG.txt 	nouveau fichier : Tomb Raider 4 - The Last Revelation/gamepad profiles/tr4tlr_gamepad_readme.html 	nouveau fichier : Tomb Raider 4 - The Last Revelation/gamepad profiles/tr4tlr_steam.sccprofile 	nouveau fichier : Tomb Raider 4 - The Last Revelation/gamepad profiles/tr4tlr_x360.gamecontroller.amgp 	nouveau fichier : Tomb Raider 4 - The Last Revelation/tr4tlr.bmp

--- Tomb Raider 3 - The Lost Artefact/clean.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-cd "drive_c/TR3TLA/"
-chmod -R u-x,g-x,o-x,u+X,u+w,g+r *
-rm -f -r DIRECTX Script autorun* data1* DSETUP* _* setup.* layout.bin os.dat tomb3.exe 3Dfx Doc MS DATA.TAG lang.dat Setup.exe SETUP.INI
