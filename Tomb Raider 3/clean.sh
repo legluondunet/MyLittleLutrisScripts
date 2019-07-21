a22d6865015a865cbf85c616321decf7186803c6
legluondunet <legluondunet@free.fr>
2019-07-21 16:23:23 +0200

    modifié :         Tomb Raider 3/clean.sh

--- Tomb Raider 3/clean.sh
+++ Tomb Raider 3/clean.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 cd "drive_c/GOG Games/Tomb Raider 3/"
-rm -f -r __support/ gog* webcache.zip 3Dfx Doc MS os.dat layout.bin  lang.dat data.bin DATA.TAG
+rm -f -r __support/ gog* webcache.zip 3Dfx Doc MS os.dat layout.bin  lang.dat data.bin DATA.TAG config.txt
