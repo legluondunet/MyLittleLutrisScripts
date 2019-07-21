baa11f04918dda8ff07a66abb11d6805a57a9068
legluondunet <legluondunet@free.fr>
2019-07-21 11:13:51 +0200

    modifié :         Tomb Raider 2/clean.sh

--- Tomb Raider 2/clean.sh
+++ Tomb Raider 2/clean.sh
@@ -7,11 +7,10 @@ do
 done
 
 cd ../data
-rm -f -r auto* data1.cab data1.hdr DATA.TAG _INST32I.EX_ _ISDel.exe lang.dat layout.bin os.dat setup.bmp setup.ins setup.lid _Setup.dll Setup.exe SETUP.INI _sys1.cab _sys1.hdr _user1.cab _user1.hdr dsetup* data language.dat _* setup.BMP SETUP.EXE tombpc.dat
+rm -f -r auto* data1.cab data1.hdr DATA.TAG _INST32I.EX_ _ISDel.exe lang.dat layout.bin os.dat setup.bmp setup.ins setup.lid _Setup.dll Setup.exe SETUP.INI _sys1.cab _sys1.hdr _user1.cab _user1.hdr dsetup* data language.dat _* setup.BMP SETUP.EXE
 
 cd ..
-rm -f -r __support/ gog* webcache.zip music/*.ogg music/ffmpeg.exe music/ogg2mp3.bat audio/ 3Dfx MS Doc
-mv music audio
+rm -f -r __support/ gog* webcache.zip audio/ 3Dfx MS Doc
 
 cd ..
 cp -r "Tomb Raider 2" "Tomb Raider 2 Golden Mask"
