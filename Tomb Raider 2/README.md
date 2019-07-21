86aff2f54d9e8b21ecf8e11331c28f1b6e84c700
legluondunet <legluondunet@free.fr>
2019-07-21 10:38:52 +0200

    modifié :         Tomb Raider 2/README.md 	modifié :         Tomb Raider 2/Tomb Raider II - GOG.txt 	modifié :         Tomb Raider 2/clean.sh 	nouveau fichier : Tomb Raider 2/gm.zip

--- Tomb Raider 2/README.md
+++ Tomb Raider 2/README.md
@@ -9,7 +9,7 @@ TR2Main by Arsunt: https://github.com/Arsunt/TR2Main
 innoextract: http://constexpr.org/innoextract/
 ffmpeg: https://ffmpeg.org/
 other sources: https://www.tombraiderforums.com
-Thanks to the people who contributed to help us playing this game in the best conditions.
+Thanks to the people who contributed to play this game in the best conditions.
 
 Technical notes:
 - as TR2 has insufficient controller support, I invite you to use a keymapper. You will find profiles for x360 (Antimicro) and Steam Controller (sc-controller) in the "gamepad profiles" folder.
--- Tomb Raider 2/clean.sh
+++ Tomb Raider 2/clean.sh
@@ -1,12 +1,23 @@
 #!/bin/bash
-cd "drive_c/GOG Games/Tomb Raider 2/audio
+
+cd "drive_c/GOG Games/Tomb Raider 2/music"
 for file in Track*.mp3
 do
 	mv -v "$file" "${file/Track/}"
 done
+
 cd ../data
-rm -f -r data1.cab data1.hdr DATA.TAG _INST32I.EX_ _ISDel.exe lang.dat layout.bin os.dat setup.bmp setup.ins setup.lid _Setup.dll Setup.exe SETUP.INI _sys1.cab _sys1.hdr _user1.cab _user1.hdr dsetup* data language.dat _* setup.BMP SETUP.EXE
+rm -f -r auto* data1.cab data1.hdr DATA.TAG _INST32I.EX_ _ISDel.exe lang.dat layout.bin os.dat setup.bmp setup.ins setup.lid _Setup.dll Setup.exe SETUP.INI _sys1.cab _sys1.hdr _user1.cab _user1.hdr dsetup* data language.dat _* setup.BMP SETUP.EXE tombpc.dat
+
 cd ..
-rm -f -r __support/ gog* webcache.zip music/*.ogg music/ffmpeg.exe music/ogg2mp3.bat audio/
+rm -f -r __support/ gog* webcache.zip music/*.ogg music/ffmpeg.exe music/ogg2mp3.bat audio/ 3Dfx MS Doc
 mv music audio
-exit
+
+cd ..
+cp -r "Tomb Raider 2" "Tomb Raider 2 Golden Mask"
+
+cd "Tomb Raider 2 Golden Mask"
+rm -f -r Tomb2.gog "Tomb Raider"* *.BAT manual.pdf CHANGELOG.html COPYING.txt "gamepad profiles" "ExtraOptions" QuickGuide.html readme.txt fmv dgVoodoo_original.conf
+
+cd "Tomb Raider 2 Golden Mask/data"
+rm -f -r *
