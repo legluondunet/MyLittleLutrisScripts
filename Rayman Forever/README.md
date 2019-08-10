33cec15479df01f7d0dbe0a03c0c8fa63aca6b83
legluondunet <legluondunet@free.fr>
2019-08-10 20:27:32 +0200

    modifié :         Rayman Forever/README.md 	modifié :         Rayman Forever/Rayman Forever - GOG.txt 	nouveau fichier : Rayman Forever/Rayman_GOG_Complete_Soundtrack.zip 	modifié :         Rayman Forever/clean.sh

--- Rayman Forever/README.md
+++ Rayman Forever/README.md
@@ -1,15 +1,11 @@
-# Rayman - CD DOS - DOSBOX
+# Rayman Forever - GOG - DOSBOX
 
 Description:
-This script will install "Rayman" DOS CD edition for Dosbox.
+This script will install "Rayman Forever" GOG edition for Dosbox.
 During install please let all options by default.
 Big thanks to people who gave their time to permit us playing this game in the best conditions.
 
 Technical notes:
-- This script requires cdrdao and toc2cue to be installed. 
-On Debian and Ubuntu type in a terminal: 
-sudo apt-get install cdrdao
-- You will need the CD only one time during install
 - x360 gamepad support
 - Please report issue concerning this script on my github page:
 https://github.com/legluondunet/MyLittleLutrisScripts/
--- Rayman Forever/clean.sh
+++ Rayman Forever/clean.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-cd "GOG Games/Rayman Forever"
 rm -f -r app/ commonappdata/ DOSBOX/ __redist/ __support/ tmp/ goggame-1207658919.* 
 sed -i 's=Music\\=Music/=g' game.ins
 cd Dosbox
-mv dosboxRayman.conf Rayman_dosbox.conf
-mv dosboxRayman_single.conf Rayman_single_dosbox.conf
-sed -i 's=..\\=../=g' Rayman_single_dosbox.conf
-sed -i 's/output=overlay/output=opengl/g' Rayman_dosbox.conf
-sed -i 's/fullscreen=true/fullscreen=false/g' Rayman_dosbox.conf
-     
+mv dosboxRayman.conf Rayman_Forever_dosbox.conf
+mv dosboxRayman_single.conf Rayman_Forever_single_dosbox.conf
+sed -i 's=..\\=../=g' Rayman_Forever_single_dosbox.conf
+sed -i 's/output=overlay/output=opengl/g' Rayman_Forever_dosbox.conf
+sed -i 's/fullscreen=true/fullscreen=false/g' Rayman_Forever_dosbox.conf
+sed -i 's/core=normal/core=auto/g' Rayman_Forever_dosbox.conf
+sed -i 's/cycles=80000/cycles=auto/g' Rayman_Forever_dosbox.conf
