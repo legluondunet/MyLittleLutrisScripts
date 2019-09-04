d4c48b685478ab258e9d31999530fd79c6da68c0
legluondunet <legluondunet@free.fr>
2019-09-05 01:06:55 +0200

    nouveau fichier : Syberia/clean.sh

---
+++ Syberia/clean.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+lang=$1
+echo $lang
+
+cd drive_c
+mkdir "GOG Games"
+mv Syberia "GOG Games"
+cd "GOG Games/Syberia"
+rm -f -r gog* webcache.zip __support
+
+
+cd ../../tmp/$lang
+echo installing $lang files...
+cp -R * "../../GOG Games/Syberia/"
+
+
+rm -f -r tmp
