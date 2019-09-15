737d513cd5faeaeaa2319b587f2705273e3b8143
legluondunet <legluondunet@free.fr>
2019-09-16 00:06:59 +0200

    modifié :         Toonstruck/Toonstruck - CD - ScummVM.txt 	modifié :         Toonstruck/clean.sh

--- Toonstruck/clean.sh
+++ Toonstruck/clean.sh
@@ -2,8 +2,8 @@
 chmod -R +w *
 find .  -type d -print0 | xargs -0n 1 bash -c 's=$(dirname "$0")/$(basename "$0"); d=$(dirname "$0")/$(basename "$0"|tr "[a-z]" "[A-Z]"); mv -f "$s" "$d"';
 find .  -type f -print0 | xargs -0n 1 bash -c 's=$(dirname "$0")/$(basename "$0"); d=$(dirname "$0")/$(basename "$0"|tr "[a-z]" "[A-Z]"); mv -f "$s" "$d"';
-cp -Rf TOONSTRUCKCD2/* TOONSTRUCKCD1
-mv TOONSTRUCKCD1 TOONSTRUCKCD
+cp -Rf TOONSTRUCKCD2/* TOONSTRUCKCD1/
 rm -rf TOONSTRUCKCD2
+mv TOONSTRUCKCD1 TOONSTRUCKCD
 cd TOONSTRUCKCD
-#rm -f -r burst/ auto* dos4gw.exe install.bat readme.pif setup.* readme.exe
+rm -f -r BURST/ AUTOPLAY.EXE AUTORUN.INF DOS4GW.EXE INSTALL.BAT README.PIF README.EXE SETUP.EXE  SETUP.INI  SETUP.PIF
