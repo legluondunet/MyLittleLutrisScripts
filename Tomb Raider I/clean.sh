26dfbc543c5242d1a6d71d42cf44db43cb372abd
legluondunet <legluondunet@free.fr>
2019-09-28 16:09:06 +0200

    supprimé :        Diablo/devilutionx 	nouveau fichier : Tomb Raider I/TITLEH.zip 	supprimé :        Tomb Raider I/TITLEHTR1UB.PCX 	supprimé :        Tomb Raider I/clean.sh

--- Tomb Raider I/clean.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-if [ -d "drive_c/GOG Games/app/Tomb Raider 1" ]; then
-mv  "drive_c/GOG Games/app/Tomb Raider 1" "drive_c/GOG Games/Tomb Raider 1"
-elif  [ -d "drive_c/GOG Games/app/TOMBRAID" ]; then
-mv  "drive_c/GOG Games/app/" "drive_c/GOG Games/Tomb Raider 1"
-fi
