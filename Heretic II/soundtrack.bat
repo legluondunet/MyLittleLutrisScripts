69b8b1186a1621c531032714902c6455f93e3839
legluondunet <legluondunet@free.fr>
2019-09-29 17:31:36 +0200

    modifié :         Heretic II/HERETICM2.BAT 	modifié :         Heretic II/soundtrack.bat

--- Heretic II/soundtrack.bat
+++ Heretic II/soundtrack.bat
@@ -2,9 +2,13 @@
 color 0d
 title "installing Heretic II soundtracks..."
 c:
-cd c:\tmp
+cd "c:\Program Files (x86)\Heretic II\tmp3"
+
 for %%A in (d e f g h i) do (
 dir %%A:|bin\grep -c "HERETIC">%%A.txt)
+
+echo pause 1b
+pause
 
 set driv=
 set /p drivd=<d.txt
@@ -29,20 +33,26 @@ set driv=g:
 goto :rip )
 goto :notfound
 
+
 :notfound
 echo le CD HERETIC est introuvable
 goto :eof
 
+
 :rip
 for %%x in (2,3,4,5,6,7,8,9) do (
-mplayer\mplayer -cdrom-device %driv% -ao pcm:file=track0%%x.wav cdda://%%x )
+mplayer -cdrom-device %driv% -ao pcm:file=track0%%x.wav cdda://%%x )
 for %%y in (10,11,12,13,14,15) do (
-mplayer\mplayer -cdrom-device %driv% -ao pcm:file=track%%y.wav cdda://%%y )
+mplayer -cdrom-device %driv% -ao pcm:file=track%%y.wav cdda://%%y )
+
 
 mkdir gapless
 for %%a IN (*.wav) DO (
-	echo %%a & bin\sox "%%a" "gapless\%%a" fade 0 -2.6 0.5 & bin\sox "gapless\%%a" "%%~na.flac"
+	echo %%a & sox "%%a" "gapless\%%a" fade 0 -2.6 0.5 & sox "gapless\%%a" "%%~na.flac"
 )
  
+
 mkdir "c:\Program Files (x86)\Heretic II\music"
+
+
 xcopy /Y *.flac "c:\Program Files (x86)\Heretic II\music"
