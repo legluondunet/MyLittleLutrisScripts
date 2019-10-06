688a0c78ae7d1fe25cb027727d5dbb1249628173
legluondunet <legluondunet@free.fr>
2019-10-06 19:08:19 +0200

    modifié :         Age of Empires/Age of Empires - Gold Edition - CD.txt 	modifié :         Age of Empires/Age of Empires - Rise of Rome - CD.txt 	modifié :         Age of Empires/clean.sh 	modifié :         X-Men Origins - Wolverine/X-Men Origins - Wolverine - DVD.txt

--- Age of Empires/clean.sh
+++ Age of Empires/clean.sh
@@ -10,17 +10,17 @@ ls | grep -iw avi | xargs rm -fr
 ls | grep -i empires.exe | xargs -I {} mv {} empires.exe.bak
 ls | grep -i language.dll | xargs -I {}  mv {} language.dll.bak
 
-if [ $aoeversion = "gold" ] 
+if [ $aoeversion = "gold" ] || [ $aoeversion = "ror" ]
 then
 ls | grep -i empiresx.exe | xargs -I {}  mv {} empiresx.exe.bak
 else
-echo "Cette version n'est pas une Gold édition"
+echo "Cette version n'est pas une Gold édition ni l'extension ROR"
 fi
 
 dirtmp="../../../tmp"
 
 echo "update de la version $aoeversion en $lang"
-if [ $aoeversion = "gold" ] 
+if [ $aoeversion = "gold" ] || [ $aoeversion = "ror" ]
 then
 cp "$dirtmp/exe/"* .
 elif [ $aoeversion = "standard" ] 
@@ -30,7 +30,9 @@ fi
 
 cp "$dirtmp/$lang/"* .
 echo "$dirtmp/exe/" "$dirtmp/$lang/"
-rm -f -r "$dirtmp"
+#rm -f -r "$dirtmp"
 mkdir avi
-cp "$cdpath/game/avi/"* avi/
+avipath=$(find "$cdpath" |grep -i -m1 avi)
+echo "$avipath"
+cp "$avipath/"* avi/
 
