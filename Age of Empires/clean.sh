33ce3c768ed61d3ca54d23ee0058d6d373aad6ea
Christoph Jabs <christoph@jabs-family.de>
2019-10-17 19:39:03 +0200

    Changed script to complete successfully
    Script now completes successfully, even if installscript does not exist

--- Age of Empires/clean.sh
+++ Age of Empires/clean.sh
@@ -1,40 +1,43 @@
 #!/bin/bash
 
-lang=$1
-cdpath=$2
-aoeversion=$3
-
-cd "drive_c/Program Files (x86)/Microsoft Games/Age of Empires"
-
-ls | grep -iw avi | xargs rm -fr
-ls | grep -i empires.exe | xargs -I {} mv {} empires.exe.bak
-ls | grep -i language.dll | xargs -I {}  mv {} language.dll.bak
-
-if [ $aoeversion = "gold" ] || [ $aoeversion = "ror" ]
-then
-ls | grep -i empiresx.exe | xargs -I {}  mv {} empiresx.exe.bak
-else
-echo "Cette version n'est pas une Gold édition ni l'extension ROR"
-fi
-
-dirtmp="../../../tmp"
-
-echo "update de la version $aoeversion en $lang"
-if [ $aoeversion = "gold" ] || [ $aoeversion = "ror" ]
-then
-cp "$dirtmp/exe/"* .
-elif [ $aoeversion = "standard" ] 
-then
-cp "$dirtmp/exe/empires.exe" .
-fi
-
-cp "$dirtmp/$lang/"* .
-echo "$dirtmp/exe/" "$dirtmp/$lang/"
-#rm -f -r "$dirtmp"
-mkdir avi
-avipath=$(find "$cdpath" |grep -i -m1 avi)
-echo "$avipath"
-cp "$avipath/"* avi/
+#lang=$1
+#cdpath=$2
+#aoeversion=$3
+#
+#cd "drive_c/Program Files (x86)/Microsoft Games/Age of Empires"
+#
+#ls | grep -iw avi | xargs rm -fr
+#ls | grep -i empires.exe | xargs -I {} mv {} empires.exe.bak
+#ls | grep -i language.dll | xargs -I {}  mv {} language.dll.bak
+#
+#if [ $aoeversion = "gold" ] || [ $aoeversion = "ror" ]
+#then
+#ls | grep -i empiresx.exe | xargs -I {}  mv {} empiresx.exe.bak
+#else
+#echo "Cette version n'est pas une Gold édition ni l'extension ROR"
+#fi
+#
+#dirtmp="../../../tmp"
+#
+#echo "update de la version $aoeversion en $lang"
+#if [ $aoeversion = "gold" ] || [ $aoeversion = "ror" ]
+#then
+#cp "$dirtmp/exe/"* .
+#elif [ $aoeversion = "standard" ] 
+#then
+#cp "$dirtmp/exe/empires.exe" .
+#fi
+#
+#cp "$dirtmp/$lang/"* .
+#echo "$dirtmp/exe/" "$dirtmp/$lang/"
+##rm -f -r "$dirtmp"
+#mkdir avi
+#avipath=$(find "$cdpath" |grep -i -m1 avi)
+#echo "$avipath"
+#cp "$avipath/"* avi/
 
 # Delete installation batch file
-[ -e installaoe.bat ] && rm installaoe.bat
+if [ -e installaoe.bat ]
+then
+  rm installaoe.bat
+fi
