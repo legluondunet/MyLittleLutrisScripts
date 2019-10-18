72c5891a7a22b4036eb71f728ca09a29af331303
legluondunet <legluondunet@free.fr>
2019-10-18 12:14:06 +0200

    nouveau fichier : Age of Empires/aoe1.bat

---
+++ Age of Empires/aoe1.bat
@@ -0,0 +1,52 @@
+@echo off
+color 4E
+goto launcher
+
+
+cd aoecovers
+copy /Y 1.bmp ../aoe.bmp
+move 1.bmp 1a.bmp
+move  3.bmp 1.bmp
+move 2.bmp 3.bmp
+move 1a.bmp 2.bmp
+cd ..
+goto :launcher
+
+:launcher
+cls
+echo.
+echo.
+echo.	------------------------------------------------
+echo.			Age of Empires I
+echo.	------------------------------------------------
+echo.
+echo.	1) Age of Empires 1
+echo.	2) Age of Empires 1: Rise of Rome
+echo.	3) exit
+echo.
+echo.	------------------------------------------------
+echo.
+echo.
+
+
+
+
+choice /c123 /s /N Your choice? [1-3]:
+if errorlevel 3 goto exit
+if errorlevel 2 goto ror
+if errorlevel 1 goto aoe1
+
+:aoe1
+cls
+cd c:\dxwnd
+start dxwnd.exe /r:1
+goto exit
+
+:ror
+cls
+cd c:\dxwnd
+start dxwnd.exe /r:2
+goto exit
+
+:exit
+exit
