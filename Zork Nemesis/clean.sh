0a14e6f68697741c10675427e34628388e378a72
legluondunet <legluondunet@free.fr>
2019-11-17 01:43:28 +0100

    modifié :         Zork Nemesis/clean.sh

--- Zork Nemesis/clean.sh
+++ Zork Nemesis/clean.sh
@@ -3,7 +3,7 @@ chmod -R +w *
 find .  -depth -type d -print0 | xargs -0n 1 bash -c 's=$(dirname "$0")/$(basename "$0"); d=$(dirname "$0")/$(basename "$0"|tr "[a-z]" "[A-Z]"); mv -f "$s" "$d"';
 find .  -depth -type f -print0 | xargs -0n 1 bash -c 's=$(dirname "$0")/$(basename "$0"); d=$(dirname "$0")/$(basename "$0"|tr "[a-z]" "[A-Z]"); mv -f "$s" "$d"';
 mv ZNCD1/NEMESIS/* .
-cp -Rf * ZNCD1/ZASSETS/ .
-cp -Rf * ZNCD2/ZASSETS/ .
-cp -Rf * ZNCD3/ZASSETS/ .
-rm -f -r ZNCD1 ZNCD2 ZNCD3
+cp -Rf ZNCD2/ZASSETS/* ZNCD1/ZASSETS/*
+cp -Rf ZNCD3/ZASSETS/* ZNCD1/ZASSETS/*
+mv ZNCD1/ZASSETS/ .
+rm -f -r ZNCD1 ZNCD2 ZNCD3 QMIXER95.DLL ZNEMESIS.EXE ZNEMESIS.INI
