ab48736eababdde4508c4a34869eb5ef7ccd0557
legluondunet <legluondunet@free.fr>
2020-02-24 23:09:23 +0100

    nouveau fichier : Tomb Raider I/clean.sh 	renommé :         Tomb Raider I/tombati_multi_lang/tombati_audio_de.zip -> Tomb Raider I/tombati_multi_lang/tombati_audio_de_de.zip 	renommé :         Tomb Raider I/tombati_multi_lang/tombati_audio_en.zip -> Tomb Raider I/tombati_multi_lang/tombati_audio_en_gb.zip 	renommé :         Tomb Raider I/tombati_multi_lang/tombati_audio_fr.zip -> Tomb Raider I/tombati_multi_lang/tombati_audio_fr_fr.zip 	renommé :         Tomb Raider I/tombati_multi_lang/tombati_audio_jp.zip -> Tomb Raider I/tombati_multi_lang/tombati_audio_jp_jp.zip 	modifié :         Tomb Raider I/tombati_multi_lang/tombati_locale.zip

---
+++ Tomb Raider I/clean.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+lang=$1
+
+cd "drive_c/TOMBATI/patches"
+sed -i -e "s/localization = false/localization = true/" -e "s/localization_locale =.*/localization_locale = $lang/" tomb*
+if [[ "$lang" =~ ^(en_gb|fr_fr|de_de|jp_jp)$ ]] 
+then cd ..
+wget https://github.com/legluondunet/MyLittleLutrisScripts/raw/master/Tomb%20Raider%20I/tombati_multi_lang/tombati_audio_$lang.zip
+./7z e -y tombati_audio_$lang.zip -omusic/
