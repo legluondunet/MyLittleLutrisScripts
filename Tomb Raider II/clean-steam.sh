5d2c9e759f3819c2a8803ba3a6102c949e0235e0
legluondunet <legluondunet@free.fr>
2020-03-14 18:14:06 +0100

    modifié :         Silent Hill 2 Director's Cut/memo_version_modules.txt 	nouveau fichier : Silent Hill 2 Director's Cut/sh2_pc_menu.zip 	nouveau fichier : Tomb Raider II/TR2Main.zip 	renommé :         Tomb Raider II/clean.sh -> Tomb Raider II/clean-gog.sh 	nouveau fichier : Tomb Raider II/clean-steam.sh 	supprimé :        Tomb Raider II/zplaymm.zip

---
+++ Tomb Raider II/clean-steam.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+cd "drive_c/Tomb Raider 2/"
+mv Tomb2.exe Tomb2.bak
+rm -f -r 3Dfx Doc audio msvcp90.dll msvcr90.dll sound.dll audiere.dll MS
+cd music
+for i in *.mp3; do LD_LIBRARY_PATH=libs ./lame --decode $i "$(basename "$i" .mp3).wav"
+done
+ls {2..9}.wav | xargs -I {} mv {} 0{}
+ls *.wav | xargs -I {} mv {} Track{}
+rm -f -r *.mp3 cdparanoia flac lame metaflac versions.txt libs
+
+cd "../../Tomb Raider 2 Golden Mask"
+cp -r "../Tomb Raider 2/"* .
+rm -f -r data/ fmv/
+cp  "../Tomb Raider 2/fmv/LOGO.RPL" fmv
+
