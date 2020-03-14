83ad0006a71a2d72371a2db9c17c0461eec81fad
legluondunet <legluondunet@free.fr>
2020-03-14 18:52:32 +0100

    modifié :         Tomb Raider II/clean-steam.sh

--- Tomb Raider II/clean-steam.sh
+++ Tomb Raider II/clean-steam.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 cd "drive_c/Tomb Raider 2/"
-mv Tomb2.exe Tomb2.bak
+#mv Tomb2.exe Tomb2.bak
 rm -f -r 3Dfx Doc audio msvcp90.dll msvcr90.dll sound.dll audiere.dll MS
 cd music
 #for i in *.mp3; do LD_LIBRARY_PATH=libs ./lame --decode $i "$(basename "$i" .mp3).wav"
@@ -12,6 +12,6 @@ ls *.mp3 | xargs -I {} mv {} Track{}
 
 cd "../../Tomb Raider 2 Golden Mask"
 cp -r "../Tomb Raider 2/"* .
-rm -f -r data/ fmv/
+rm -f -r data/ fmv/ "gamepad profiles/"
 cp  "../Tomb Raider 2/fmv/LOGO.RPL" fmv
 
