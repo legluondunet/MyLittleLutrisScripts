c5e7e077d05f030f6176a8d1a8cc97ccc4d338d0
legluondunet <legluondunet@free.fr>
2020-03-15 19:07:14 +0100

    modifié :         Tomb Raider II/TR2M.BAT 	modifié :         Tomb Raider II/Tomb Raider II - GOG.txt 	modifié :         Tomb Raider II/Tomb Raider II - Steam.txt 	modifié :         Tomb Raider II/clean-gog.sh 	modifié :         Tomb Raider II/clean-steam.sh

--- Tomb Raider II/clean-steam.sh
+++ Tomb Raider II/clean-steam.sh
@@ -1,19 +1,20 @@
 #!/bin/bash
 
+mkdir -p "drive_c/Tomb Raider 2 Golden Mask/"
+
 cd "drive_c/Tomb Raider 2/"
-#mv Tomb2.exe Tomb2.bak
-rm -f -r 3Dfx Doc audio msvcp90.dll msvcr90.dll sound.dll audiere.dll MS
-cd music
-#for i in *.mp3; do LD_LIBRARY_PATH=libs ./lame --decode $i "$(basename "$i" .mp3).wav"
-#done
-ls {2..9}.mp3 | xargs -I {} mv {} 0{}
-ls *.mp3 | xargs -I {} mv {} Track{}
-#rm -f -r *.mp3 cdparanoia flac lame metaflac versions.txt libs
-cd "../../Tomb Raider 2 Golden Mask"
-cp -r "../Tomb Raider 2/"* .
-rm -f -r data/ fmv/*
-cp  "../Tomb Raider 2/fmv/LOGO.RPL" fmv
-mv data2 data
+rm -f -r 3Dfx Doc audio msvcp90.dll msvcr90.dll sound.dll audiere.dll MS installscript.vdf QuickGuide.html
+
 cd data
 rm -f -r autorun.* *.cab setup.* *.dll *.EXE _I* SETUP* data DATA.TAG lang* layout.bin os.dat
+
+cd ../music
+ls {2..9}.mp3 | xargs -I {} mv {} 0{}
+ls *.mp3 | xargs -I {} mv {} Track{}
+
+cd "../../Tomb Raider 2 Golden Mask"
+cp -r "../Tomb Raider 2/"* .
+rm -f -r data/ fmv/* ExtraOptions dgVoodoo_original.conf Tomb2.steam data/*
+find "../Tomb Raider 2/fmv" -iname "LOGO.RPL" -exec cp "{}" fmv/ \;
+
 
