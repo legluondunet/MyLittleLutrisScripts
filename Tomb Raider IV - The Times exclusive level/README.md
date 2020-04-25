ec952b618f128c043fff4c1beb8ede7badc8a240
legluondunet <legluondunet@free.fr>
2020-04-26 00:56:53 +0200

    modifié :         Tomb Raider IV - The Times exclusive level/README.md 	nouveau fichier : Tomb Raider IV - The Times exclusive level/Tomb Raider IV - The Times exclusive level.reg 	modifié :         Tomb Raider IV - The Times exclusive level/Tomb Raider IV - The Times exclusive level.txt 	modifié :         Tomb Raider IV - The Times exclusive level/tr4times.bat

--- Tomb Raider IV - The Times exclusive level/README.md
+++ Tomb Raider IV - The Times exclusive level/README.md
@@ -1,15 +1,18 @@
 # Tomb Raider IV - The Times exclusive level
 
-Description:
-This script will install "Tomb Raider IV: The Times exclusive level" with support for modern resolution.
+### Description:
+This script will facilitate you install of this game on Linux OS:
+"Tomb Raider IV: The Times exclusive level".
 During install please let all options by default.
 This script uses different projects:
 dgVoodoo2: http://www.dege.freeweb.hu/
 Tomb Raider 2-3-4 Widescreen Patch created by Mr.Blackfour : https://tombraiders.net/stella/downloads/widescreen.html
 Other sources: https://www.tombraiderforums.com
-Thanks to the people who contributed to play this game in the best conditions.
+Thanks to the people who contributed to play this game in the best conditions on Linux platform.
 
-Technical notes:
+### Technical notes:
+- When changing graphic options, always choose "dgVoodoo" for "Graphic adaptater" and "Output settings" or you will obtain a Directx error.
+- Press P to access Pause/Exit/Options menu.
 - "Tomb Raider IV: The Times exclusive level" has insufficient controller support, I invite you to use a keymapper. You will find profiles for x360 (Antimicro) and Steam Controller (sc-controller) in the "gamepad profiles" folder.
-- Please report issue concerning this script on my github page:
+- Please report issue concerning this script on my Github page:
 https://github.com/legluondunet/MyLittleLutrisScripts/
--- Tomb Raider IV - The Times exclusive level/tr4times.bat
+++ Tomb Raider IV - The Times exclusive level/tr4times.bat
@@ -5,6 +5,7 @@ goto launcher
 
 :launcher
 c:
+cd c:\Program Files (x86)\Core Design\The Times - Exclusive Tomb Raider Level\
 cls
 echo.
 echo.
@@ -28,43 +29,55 @@ echo.
 
 
 choice /c1234567 /s /N Your choice? [1-7]:
-if errorlevel 7 goto exit
-if errorlevel 6 goto gamepad
-if errorlevel 5 goto patch
-if errorlevel 4 goto setup
-if errorlevel 3 goto clicker
-if errorlevel 2 goto jigsaw
-if errorlevel 1 goto tr4times
+if errorlevel 7 goto :exit
+if errorlevel 6 goto :gamepad
+if errorlevel 5 goto :patch
+if errorlevel 4 goto :setup
+if errorlevel 3 goto :clicker
+if errorlevel 2 goto :jigsaw
+if errorlevel 1 goto :tr4times
 
 
 :tr4times
-cls
-start c:\Program Files (x86)\Core Design\The Times - Exclusive Tomb Raider Level\tomb4.exe
+start tomb4.exe
 goto :launcher
 
 :jigsaw
-cls
-start c:\Program Files (x86)\Core Design\The Times - Exclusive Tomb Raider Level\jigsaw.exe
-goto launcher
+start jigsaw.exe
+goto :launcher
 
 :clicker
-cls
 start clicker.exe
-goto launcher
+goto :launcher
 
 :setup
 cls
+echo.
+echo.
+echo.	------------------------------------------------
+echo.	Tomb Raider IV - The Times exclusive level
+echo.	------------------------------------------------
+echo.
+echo.	4) Setup dialog box
+echo.
+echo.
+echo.	When changing graphic options, always choose dgVoodoo 
+echo.	for "Graphic adaptater" and "Output settings" 
+echo.	or you will obtain a Directx error.
+echo.
+echo.	------------------------------------------------
+echo.
+echo.
 start tomb4.exe -setup
-goto launcher
+pause
+goto :launcher
 
 :patch
-cls
 start TombRaider234_Patch.exe
 pause
-goto launcher
+goto :launcher
 
 :gamepad
-cls
 start explorer c:\Program Files (x86)\Core Design\The Times - Exclusive Tomb Raider Level\gamepad profiles\
 start iexplore c:\Program Files (x86)\Core Design\The Times - Exclusive Tomb Raider Level\gamepad profiles\tr4_gamepad_readme.html
 goto launcher
