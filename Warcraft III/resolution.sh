0cca48b3a3d65078263a4b2200e0861a7d4e4b7e
legluondunet <legluondunet@free.fr>
2020-04-26 23:05:46 +0200

    modifié :         Warcraft III/README.md 	nouveau fichier : Warcraft III/Warcraft III  - Reign Of Chaos - CD.txt 	modifié :         Warcraft III/resolution.sh 	modifié :         Warcraft III/warcraft_iii_cd.bat 	modifié :         Warcraft III/warcraft_iii_cd_utf8.reg

--- Warcraft III/resolution.sh
+++ Warcraft III/resolution.sh
@@ -12,7 +12,17 @@ echo $h en hexa: $hb soit ${#hb} caractères
 
 cd drive_c/tmp/
 
-sed -i -e 's/"windowwidth"\=dword\:.*/"windowwidth"\=dword\:'$wb'/' -e 's/"windowheight"\=dword\:.*/"windowheight"\=dword\:'$hb'/' $file
+if [ $file == "warcraft_iii_cd_utf8.reg" ]; then
+echo "fichier reconnu: warcraft_iii_cd_utf8.reg"
+echo "la variable file: " $file
+sed -i -e 's/"reswidth"\=dword\:.*/"reswidth"\=dword\:'$wb'/' -e 's/"resheight"\=dword\:.*/"resheight"\=dword\:'$hb'/' $file
+fi
 
+if [ $file == "warcraft_iii_ptr_utf8.reg" ]; then
+echo "fichier reconnu: warcraft_iii_ptr_utf8.reg"
+echo "la variable file: " $file
+sed -i -e 's/"windowwidth"\=dword\:.*/"windowwidth"\=dword\:'$wb'/' -e 's/"windowheight"\=dword\:.*/"windowheight"\=dword\:'$hb'/' $file
+fi
 
 iconv -f utf-8 -t utf16le $file -o warcraft_iii.reg
+
--- Warcraft III/warcraft_iii_cd.bat
+++ Warcraft III/warcraft_iii_cd.bat
@@ -1,16 +1,12 @@
 @echo off
 TITLE Warcraft III
-color 09
+color 07
 
 c:
 cd \
 copy warcraft_iiib.bmp warcraft_iii.bmp 
 copy warcraft_iiia.bmp warcraft_iiib.bmp
 copy warcraft_iii.bmp warcraft_iiia.bmp 
-
-IF NOT EXIST "%PROGRAMDATA%\Blizzard Entertainment\Warcraft III\" md "%PROGRAMDATA%\Blizzard Entertainment\\Warcraft III Public Test\"
-IF NOT EXIST "%PROGRAMDATA%\Blizzard Entertainment\Warcraft III Public Test\roc.w3k" goto :key
-IF NOT EXIST "%PROGRAMDATA%\Blizzard Entertainment\Warcraft III Public Test\user.w3k" goto :key
 
 goto :launcher
 
@@ -19,12 +15,11 @@ cls
 echo.
 echo.
 echo.	------------------------------------------------
-echo.	      Warcraft III Blizzard digital edition
+echo.	    Warcraft III Blizzard CD Classic edition
 echo.	------------------------------------------------
 echo.
-echo.	1) Play
-rem echo.	2) Check update
-echo.	2) insert or change Warcraft III licence key
+echo.	1) Play ROC
+echo.	2) Play TFT
 echo.	3) exit
 echo.
 echo.	------------------------------------------------
@@ -33,43 +28,16 @@ echo.
 
 choice /c123 /s /N Your choice? [1-3]:
 if errorlevel 3 goto :exit
-if errorlevel 2 goto :key
-rem if errorlevel 2 goto :update
-if errorlevel 1 goto :play
+if errorlevel 2 goto :TFT
+if errorlevel 1 goto :ROC
 
-:key
-cls
-echo.
-echo.
-echo.	------------------------------------------------
-echo.	      Warcraft III Blizzard digital edition
-echo.	------------------------------------------------
-echo.
-
-set Output="%PROGRAMDATA%\Blizzard Entertainment\Warcraft III Public Test\"
-
-set /p VarOne=	Please enter Warcraft III Reign of Chaos key: 
-echo.
-echo.
-echo. 
-set /p VarTwo=	Please enter a name (your name, your pet name, your computer name...): 
-
-REM enter your desired output here
-echo %VarOne% > %Output%\roc.w3k
-echo %VarTwo% > %Output%\user.w3k
-echo. 
-echo. Done! Now press a key to return to main menu.
-pause
+:ROC
+start "" "c:\program files (x86)\warcraft iii\warcraft iii.exe"
 goto :launcher
 
-:play
-start "" "c:\program files\warcraft iii Public Test\x86_64\warcraft iii.exe" > nul
-goto :exit
-
-rem :update
-rem start "" "c:\programdata\battle.net\agent\agent.exe"
-rem ping 127.0.0.1 -n 2 > nul
-rem start "" "c:\program files\warcraft iii\warcraft III launcher.exe"
+:TFT
+start "" "c:\program files (x86)\warcraft iii\warcraft iii.exe"
+goto :launcher
 
 :exit
 exit
