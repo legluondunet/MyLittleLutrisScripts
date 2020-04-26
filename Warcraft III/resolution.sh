860b653aa1d954b22ed76a0a5da701325a88913b
legluondunet <legluondunet@free.fr>
2020-04-26 19:36:25 +0200

    modifié :         Warcraft III/resolution.sh 	renommé :         Warcraft III/warcraft_iii.bat -> Warcraft III/warcraft_iii_cd.bat 	nouveau fichier : Warcraft III/warcraft_iii_ptr.bat

--- Warcraft III/resolution.sh
+++ Warcraft III/resolution.sh
@@ -2,7 +2,7 @@
 
 w=$1
 h=$2
-$file=$3
+file=$3
 
 wb=$(echo 'obase=16;' $w | bc)
 hb=$(echo 'obase=16;' $h | bc)
@@ -12,7 +12,7 @@ echo $h en hexa: $hb soit ${#hb} caractères
 
 cd drive_c/tmp/
 
-sed -i -e 's/"windowwidth"\=dword\:.*/"windowwidth"\=dword\:'$wb'/' -e 's/"windowheight"\=dword\:.*/"windowheight"\=dword\:'$hb'/' $file_utf8.reg
+sed -i -e 's/"windowwidth"\=dword\:.*/"windowwidth"\=dword\:'$wb'/' -e 's/"windowheight"\=dword\:.*/"windowheight"\=dword\:'$hb'/' $file
 
 
-iconv -f utf-8 -t utf16le $file_utf8.reg -o $file.reg
+iconv -f utf-8 -t utf16le $file -o warcraft_iii.reg
