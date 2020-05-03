359125cd145576ab8322fa3e898c69e466c62f31
legluondunet <legluondunet@free.fr>
2020-05-03 09:51:27 +0200

    renommé :         L.A.Noire/L.A.Noire - Steam.txt -> L.A.Noire/L.A.Noire - Windows Steam Standalone.txt 	modifié :         L.A.Noire/README.md 	modifié :         L.A.Noire/lanoire.bat 	supprimé :        Loom/Indiana Jones and the Fate of Atlantis - GOG.txt 	nouveau fichier : Loom/LOOM - GOG.txt 	modifié :         Loom/README.md 	supprimé :        Loom/indi4_multi5.zip 	nouveau fichier : Loom/loom_multi_5.tar.xz

--- L.A.Noire/lanoire.bat
+++ L.A.Noire/lanoire.bat
@@ -1,11 +1,21 @@
 @echo off
 c:
+
+rem avoid dotnet to install at each L.A. Noire launch
 if exist "c:\Program Files (x86)\Steam\steamapps\common\L.A.Noire\3rd\dotnetfx35.exe" move /y "c:\Program Files (x86)\Steam\steamapps\common\L.A.Noire\3rd\dotnetfx35.exe" "c:\Program Files (x86)\Steam\steamapps\common\L.A.Noire\3rd\dotnetfx35.bak"
 
+rem copy patch Windows 8.1
+if not exist "c:\Program Files (x86)\Steam\steamapps\common\L.A.Noire\final\pc" mkdir "c:\Program Files (x86)\Steam\steamapps\common\L.A.Noire\final\pc"
+if not exist "c:\Program Files (x86)\Steam\steamapps\common\L.A.Noire\final\pc\out.wad.pc" copy /y "c:\tmp\pc\out.wad.pc" "c:\Program Files (x86)\Steam\steamapps\common\L.A.Noire\final\pc\"
+if not exist "c:\Program Files (x86)\Steam\steamapps\common\L.A.Noire\final\pc\cases.big.pc" copy /y "c:\tmp\pc\cases.big.pc" "c:\Program Files (x86)\Steam\steamapps\common\L.A.Noire\final\pc\"
+
+rem copy settings compatible with Wine
 for /f "tokens=2* delims= " %%a in ('reg query "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Shell Folders" /v personal') do set MyDocs="%%b"
 echo My Documents points to %MyDocs%
 if not exist %MyDocs%"\Rockstar Games\L.A. Noire\" mkdir %MyDocs%"\Rockstar Games\L.A. Noire\"
 if not exist %MyDocs%"\Rockstar Games\L.A. Noire\settings.ini" copy "c:\tmp\settings.ini" %MyDocs%"\Rockstar Games\L.A. Noire\"
+
+rem launch game
 "c:\program files (x86)\steam\steam.exe" -no-browser steam://run/110800
 
 
--- Loom/README.md
+++ Loom/README.md
@@ -1,11 +1,12 @@
-# Indiana Jones and the Fate of Atlantis - GOG - ScummVM
+# Indiana Jones and the Last Crusade - GOG - ScummVM
 
-### Description:
-This script will install "Indiana Jones and the Fate of Atlantis" GOG edition for ScummVM.
+Description:
+This script will install "Indiana Jones and the Last Crusade" GOG edition for ScummVM.
 During install please let all options by default.
 Thanks to the people who contributed to play this game in the best conditions.
 
-### Technical notes:
-- Multilanguage: you could choose your preferred subtitles language during install between english, french, italian spanish and german. The voices will still stay in english.
+Technical notes:
+- This script is only compatible with the game installation file for Windows in exe format provided by GOG.
+- Multilanguage: you could choose your preferred subtitles language during install between english (default), french, italian, spanish and german.
 - Please report issue concerning this script on my Github page:
 https://github.com/legluondunet/MyLittleLutrisScripts/
