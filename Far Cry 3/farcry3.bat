4854bcb00a873cde597ec4c571a189f3d1c39bb9
legluondunet <legluondunet@free.fr>
2020-05-08 22:17:40 +0200

    nouveau fichier : Far Cry 3/GamerProfile.xml 	modifié :         Far Cry 3/farcry3.bat 	nouveau fichier : Far Cry 3/settings.yml

--- Far Cry 3/farcry3.bat
+++ Far Cry 3/farcry3.bat
@@ -4,6 +4,9 @@ c:
 for /f "tokens=2* delims= " %%a in ('reg query "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Shell Folders" /v personal') do set MyDocs="%%b"
 echo My Documents points to %MyDocs%
 
+if not exist %MyDocs%"\My Games\Far Cry 3" mkdir %MyDocs%"\My Games\Far Cry 3"
+if not exist %MyDocs%"\My Games\Far Cry 3" copy "c:\tmp\GamerProfile.xml" %MyDocs%"\My Games\Far Cry 3"
+
 "c:\program files (x86)\steam\steam.exe" -no-browser steam://run/220240
 
 
