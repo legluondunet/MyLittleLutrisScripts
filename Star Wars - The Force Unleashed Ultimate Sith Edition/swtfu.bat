e177411ca1e41400e04af0ef0c3110957911bc54
legluondunet <legluondunet@free.fr>
2020-05-10 19:53:53 +0200

    modifié :         Star Wars - The Force Unleashed Ultimate Sith Edition/Config.xml 	supprimé :        Star Wars - The Force Unleashed Ultimate Sith Edition/Config.xml.x360 	modifié :         Star Wars - The Force Unleashed Ultimate Sith Edition/swtfu.bat

--- Star Wars - The Force Unleashed Ultimate Sith Edition/swtfu.bat
+++ Star Wars - The Force Unleashed Ultimate Sith Edition/swtfu.bat
@@ -6,8 +6,12 @@ echo My Documents points to %MyDocs%
 
 if not exist "%appdata%\Aspyr\Star Wars Le Pouvoir de la Force" mkdir "%appdata%\Aspyr\Star Wars Le Pouvoir de la Force\"
 if not exist "%appdata%\Aspyr\Star Wars The Force Unleashed\" mkdir "%appdata%\Aspyr\Star Wars The Force Unleashed\"
-if not exist "%appdata%\Aspyr\Star Wars Le Pouvoir de la Force\Config.xml" copy c:\tmp\Config.xml "%appdata%\Aspyr\Star Wars Le Pouvoir de la Force\"
-if not exist "%appdata%\Star Wars The Force Unleashed\Config.xml" copy copy c:\tmp\Config.xml "%appdata%\Aspyr\Star Wars The Force Unleashed\"
+if not exist "%appdata%\Aspyr\Star Wars Le Pouvoir de la Force\Config.xml" copy "c:\tmp\Config.xml" "%appdata%\Aspyr\Star Wars Le Pouvoir de la Force\"
+if not exist "%appdata%\Star Wars The Force Unleashed\Config.xml" copy "c:\tmp\Config.xml" "%appdata%\Aspyr\Star Wars The Force Unleashed\"
+
+if exist "c:\Program Files (x86)\Steam\steamapps\Common\Star Wars The Force Unleashed\Support\dotnet\dotnetfx35.exe" move /Y "c:\Program Files (x86)\Steam\steamapps\Common\Star Wars The Force Unleashed\Support\dotnet\dotnetfx35.exe" "c:\Program Files (x86)\Steam\steamapps\Common\Star Wars The Force Unleashed\Support\dotnetfx35.bak"
+
+
 echo appdata points to %appdata%
 
 
