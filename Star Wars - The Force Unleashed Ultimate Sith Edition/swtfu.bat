d3ce46539ab4244cf41121d14f2147a158229f3c
legluondunet <legluondunet@free.fr>
2020-05-10 02:26:11 +0200

    modifié :         Star Wars - The Force Unleashed Ultimate Sith Edition/swtfu.bat

--- Star Wars - The Force Unleashed Ultimate Sith Edition/swtfu.bat
+++ Star Wars - The Force Unleashed Ultimate Sith Edition/swtfu.bat
@@ -4,7 +4,7 @@ c:
 for /f "tokens=2* delims= " %%a in ('reg query "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Shell Folders" /v personal') do set MyDocs="%%b"
 echo My Documents points to %MyDocs%
 
-"c:\program files (x86)\steam\steam.exe" -no-browser steam://run/367500
+"c:\program files (x86)\steam\steam.exe" -no-browser steam://run/32430
 
 
 
