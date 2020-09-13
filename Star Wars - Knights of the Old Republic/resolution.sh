fb832bbfa9de82543c3924e1459aa92d4549c648
legluondunet <legluondunet@free.fr>
2020-09-13 17:14:09 +0200

    modifié :         Star Wars - Knights of the Old Republic/resolution.sh

--- Star Wars - Knights of the Old Republic/resolution.sh
+++ Star Wars - Knights of the Old Republic/resolution.sh
@@ -4,6 +4,15 @@ ls -l
 rwidth=$1
 rheight=$2
 
+if [ $rwidth==1920] AND [ rheight==1080 ]
+then
+
+# intro cut scenes
+wget -P drive_c/tmp/ https://github.com/legluondunet/MyLittleLutrisScripts/raw/4e9b84d25fada36e04ca61fda79e1da7ca6388d3/Star%20Wars%20-%20Knights%20of%20the%20Old%20Republic/kotor_intro_cutscenes_"$rwidth"x"$rheight".7z
+7z x drive_c/tmp/kotor_intro_cutscenes_"$rwidth"x"$rheight".7z -odrive_c/tmp
+cp 'drive_c/tmp/movies_HD/'"$rwidth"'x'"$rheight"'/'* "drive_c/GOG Games/Star Wars - KotOR/movies"
+
+# mods folder
 mkdir -p "drive_c/GOG Games/Star Wars - KotOR/override"
 cp "drive_c/tmp/gui."$rwidth"x"$rheight"/"* "drive_c/GOG Games/Star Wars - KotOR/override"
 cp "drive_c/tmp/hd_ui_menupack_PV/"* "drive_c/GOG Games/Star Wars - KotOR/override"
@@ -11,7 +20,8 @@ cp "drive_c/tmp/[K1]_Main_Menu_Widescreen_Fix_v1.2/"* "drive_c/GOG Games/Star Wa
 cp "drive_c/tmp/Kotor_1_Logo_Fix_and_Bigger_HUD/"* "drive_c/GOG Games/Star Wars - KotOR/override"
 cp "drive_c/tmp/KoToR1IconMOD/"* "drive_c/GOG Games/Star Wars - KotOR/override"
 cd "drive_c/GOG Games/Star Wars - KotOR/"
+
+# widescreen patch
 cp swkotor.exe swkotor.exe.ori
 patch < ../../tmp/swkotor_gog_ws_"$rwidth"x"$rheight".patch swkotor.exe
-
-
+fi
