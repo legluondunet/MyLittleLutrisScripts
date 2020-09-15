9950085efe4556be9a117de8dec29d1b234a8c3c
legluondunet <legluondunet@free.fr>
2020-09-16 01:07:13 +0200

    modifié :         Star Wars - Knights of the Old Republic/clean.sh 	supprimé :        Star Wars - Knights of the Old Republic/kotor_widescreens_patchs_mods.7z 	nouveau fichier : Star Wars - Knights of the Old Republic/mods/KoToR1IconMOD.7z 	supprimé :        Star Wars - Knights of the Old Republic/mods/KoToR1IconMOD.rar 	supprimé :        Star Wars - Knights of the Old Republic/mods/hd_ui_menupack_PV.7z 	renommé :         Star Wars - Knights of the Old Republic/kotor_intro_cutscenes_1920x1080.7z -> Star Wars - Knights of the Old Republic/mods/kotor_intro_cutscenes_1920x1080.7z 	supprimé :        Star Wars - Knights of the Old Republic/resolution.sh

--- Star Wars - Knights of the Old Republic/clean.sh
+++ Star Wars - Knights of the Old Republic/clean.sh
@@ -3,31 +3,46 @@
 rwidth=$1
 rheight=$2
 
-# debug
-echo la résolution native du bureau est $width $rheight
+# debug 
+echo la résolution native du bureau est $rwidth $rheight
 ls -l
 
-# la résolution est-elle supportée par le jeu?
+# si la résolution est supportée
 if [ $rwidth = "1920" ] && [ $rheight = "1080" ];
 then
 echo la résolution est supportée
 
-# intro cut scenes
-wget -P drive_c/tmp/ https://github.com/legluondunet/MyLittleLutrisScripts/raw/4e9b84d25fada36e04ca61fda79e1da7ca6388d3/Star%20Wars%20-%20Knights%20of%20the%20Old%20Republic/kotor_intro_cutscenes_"$rwidth"x"$rheight".7z
-7z x -aoa drive_c/tmp/kotor_intro_cutscenes_"$rwidth"x"$rheight".7z -odrive_c/tmp
-cp 'drive_c/tmp/movies_HD/'"$rwidth"'x'"$rheight"'/'* "drive_c/GOG Games/Star Wars - KotOR/movies"
+#downloads mods
+mkdir drive_c/tmp
+cd drive_c/tmp
+wget https://github.com/legluondunet/MyLittleLutrisScripts/raw/master/Star%20Wars%20-%20Knights%20of%20the%20Old%20Republic/mods/k1hrm-1.4.7z/ 
+wget https://github.com/legluondunet/MyLittleLutrisScripts/raw/master/Star%20Wars%20-%20Knights%20of%20the%20Old%20Republic/mods/%5BK1%5D_Main_Menu_Widescreen_Fix_v1.2.7z
+wget https://github.com/legluondunet/MyLittleLutrisScripts/raw/master/Star%20Wars%20-%20Knights%20of%20the%20Old%20Republic/mods/HD%20MENU%20AND%20UI%20Art%20v%201.0.7z.001
+wget https://github.com/legluondunet/MyLittleLutrisScripts/raw/master/Star%20Wars%20-%20Knights%20of%20the%20Old%20Republic/mods/HD%20MENU%20AND%20UI%20Art%20v%201.0.7z.002
+wget https://github.com/legluondunet/MyLittleLutrisScripts/raw/master/Star%20Wars%20-%20Knights%20of%20the%20Old%20Republic/mods/hd_pc_portraits-v1.0.7z
+wget https://github.com/legluondunet/MyLittleLutrisScripts/raw/master/Star%20Wars%20-%20Knights%20of%20the%20Old%20Republic/mods/hd_npc_portraits-v2.0.7z
+wget https://github.com/legluondunet/MyLittleLutrisScripts/raw/master/Star%20Wars%20-%20Knights%20of%20the%20Old%20Republic/mods/KoToR1IconMOD.7z
+wget https://github.com/legluondunet/MyLittleLutrisScripts/raw/master/Star%20Wars%20-%20Knights%20of%20the%20Old%20Republic/mods/kotor_intro_cutscenes_"$rwidth"x"$rheight".7z
 
-# mods folder
+# extract all 7z archives
 mkdir -p "drive_c/GOG Games/Star Wars - KotOR/override"
-cp "drive_c/tmp/gui."$rwidth"x"$rheight"/"* "drive_c/GOG Games/Star Wars - KotOR/override"
-cp "drive_c/tmp/hd_ui_menupack_PV/"* "drive_c/GOG Games/Star Wars - KotOR/override"
-cp "drive_c/tmp/[K1]_Main_Menu_Widescreen_Fix_v1.2/"* "drive_c/GOG Games/Star Wars - KotOR/override"
-cp "drive_c/tmp/Kotor_1_Logo_Fix_and_Bigger_HUD/"* "drive_c/GOG Games/Star Wars - KotOR/override"
-cp "drive_c/tmp/KoToR1IconMOD/"* "drive_c/GOG Games/Star Wars - KotOR/override"
-cp "drive_c/tmp/hd_pc_portraits/"* "drive_c/GOG Games/Star Wars - KotOR/override"
-cd "drive_c/GOG Games/Star Wars - KotOR/"
+7z x -aoa .
+
+# install mods
+cp "16-by-9/gui.1920x1080/"* "drive_c/GOG Games/Star Wars - KotOR/override"
+cp "[K1]_Main_Menu_Widescreen_Fix_v1.2/For Override/"* "drive_c/GOG Games/Star Wars - KotOR/override"
+cp "HD MENU AND UI Art v 1.0/"* "drive_c/GOG Games/Star Wars - KotOR/override"
+cp "hd_pc_portraits/Override/"* "drive_c/GOG Games/Star Wars - KotOR/override"
+cp "hd_npc_portraits/Override/"* "drive_c/GOG Games/Star Wars - KotOR/override"
+cp "KoToR1IconMOD/Override"* "drive_c/GOG Games/Star Wars - KotOR/override"
+cp 'movies_HD/'"$rwidth"'x'"$rheight"'/'* "drive_c/GOG Games/Star Wars - KotOR/movies"
 
 # widescreen patch
+cd "drive_c/GOG Games/Star Wars - KotOR/"
 cp swkotor.exe swkotor.exe.ori
 patch < ../../tmp/swkotor_gog_ws_"$rwidth"x"$rheight".patch swkotor.exe
 fi
+
+# clean
+rm -f -r app commonappdata __redist __support tmp goggame-1207666283.* ;
+
