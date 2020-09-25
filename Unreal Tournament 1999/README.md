24dd37092f299195439f24f9f93cc772c03036b8
legluondunet <legluondunet@free.fr>
2020-09-25 20:32:24 +0200

    modifié :         Duke Nukem 3D/Duke Nukem 3D - Bonus levels for EDuke32.txt 	nouveau fichier : Unreal Tournament 1999/README.md 	nouveau fichier : Unreal Tournament 1999/Unreal Tournament 1999 - CD - Oldunreal patchs.txt 	nouveau fichier : Unreal Tournament 1999/clean.sh 	nouveau fichier : Unreal Tournament 1999/unreal_tournament.bat 	nouveau fichier : Unreal Tournament 1999/unreal_tournament.bmp

---
+++ Unreal Tournament 1999/README.md
@@ -0,0 +1,14 @@
+# Unreal tournament (1999) - CD - OldUnreal Patch
+
+## Description:
+This script will facilitate you install of this game on Linux OS:
+"Unreal tournament (1999)" CD edition using OldUnreal Patch project:
+https://github.com/OldUnreal/UnrealTournamentPatches/
+During install please let all options by default.
+Big thanks to people who gave their time to permit us playing this game in the best conditions on Linux platform.
+
+## Technical notes:
+- OldUnreal Patch v469a
+- You will need the CD only one time during install.
+- Please report issue concerning this script on my Github page:
+https://github.com/legluondunet/MyLittleLutrisScripts/
---
+++ Unreal Tournament 1999/unreal_tournament.bat
@@ -0,0 +1,48 @@
+@echo off
+color 0E
+c:\
+goto launcher
+
+:launcher
+cls
+echo.
+echo.
+echo.	------------------------------------------------
+echo.		Unreal Gold - GOG - version 227i
+echo.	------------------------------------------------
+echo.
+echo.	1) Play Unreal Gold (Windows version)
+echo.	2) Play Unreal Gold (Linux version)
+echo.	3) How to play Unreal Gold with a gamepad
+echo.	4) exit
+echo.
+echo.	------------------------------------------------
+echo.
+echo.
+
+
+choice /c1234 /s /N Your choice? [1-4]:
+if errorlevel 4 goto :exit
+if errorlevel 3 goto :gamepad
+if errorlevel 2 goto :playlinux
+if errorlevel 1 goto :play
+
+:play
+cd c:\GOG Games\Unreal Gold\System\
+start Unreal.exe
+goto :launcher
+
+:playlinux
+cd c:\GOG Games\Unreal Gold\
+start /unix ./UnrealLinux.sh
+goto :launcher
+
+:gamepad
+cls
+start explorer "c:\GOG Games\Unreal Gold\gamepad profiles\"
+start iexplore "c:\GOG Games\Unreal Gold\gamepad profiles\unreal_gamepad_readme.html"
+goto launcher
+
+:exit
+exit
+
