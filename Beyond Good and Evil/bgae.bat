c8d6d17d57594bca776175ad65bda0a09d3e1da7
legluondunet <legluondunet@free.fr>
2020-12-28 01:47:16 +0100

    nouveau fichier : Beyond Good and Evil/BGE widescreen fix.rar 	modifié :         Beyond Good and Evil/bgae.bat

--- Beyond Good and Evil/bgae.bat
+++ Beyond Good and Evil/bgae.bat
@@ -1,8 +1,10 @@
 echo off
 
 if exist "c:\Program Files (x86)\Ubisoft\Ubisoft Game Launcher\games\Beyond Good and Evil\SettingsApplication.exe" (
-   echo file exists
+   echo SettingsApplication.exe exists
+   "c:\Program Files (x86)\Ubisoft\Ubisoft Game Launcher\games\Beyond Good and Evil\SettingsApplication.exe"
 ) else (
-    echo file does not exist
+    echo SettingsApplication.exe does not exist
+    "c:\Program Files (x86)\Ubisoft\Ubisoft Game Launcher\UbisoftConnect.exe" uplay://launch/232/0
 )
 
