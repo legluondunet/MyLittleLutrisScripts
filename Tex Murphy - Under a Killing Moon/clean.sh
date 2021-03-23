b67c6ba692e399bea79760cb1ddb3101a93cc684
legluondunet <legluondunet@free.fr>
2021-03-23 22:42:58 +0100

    modifié :         Tex Murphy - Under a Killing Moon/clean.sh

--- Tex Murphy - Under a Killing Moon/clean.sh
+++ Tex Murphy - Under a Killing Moon/clean.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 sed -i -e 's/C.*/C \./'  -e 's/D.*/D "UAKM.GOG" -t iso/' dosboxTex3_single.conf
-sed -i -e 's/fullscreen=.*/fullscreen=false/'  -e 's/fullresolution=.*/fullresolution=desktop/' -e 's/output=.*/output=opengl/' dosboxTex3_single.conf
+sed -i -e 's/fullscreen=.*/fullscreen=false/'  -e 's/fullresolution=.*/fullresolution=desktop/' -e 's/output=.*/output=opengl/' dosboxTex3.conf
 rm -f -r  DOSBOX __support goggame-1207658767.* ;
