5f48f46fb46467d56b4406ccdb28dec66b003291
legluondunet <legluondunet@gmail.com>
2021-09-28 19:49:43 +0200

    Add files via upload

--- Quake - The Offering/quake.bat
+++ Quake - The Offering/quake.bat
@@ -17,22 +17,11 @@ echo.	3) Play QTO - Mission Pack 2 - Dissolution of Eternity
 echo.	4) Play QTO - Mission Pack 3 - Abyss of Pandemonium 
 echo.
 echo.	--------------------------------------------------------------
-echo.
-echo.	5) Play Quake - The Offering - Linux version
-echo.	6) Play QTO - Mission Pack 1 - Scourge of Armagon 
-echo.	7) Play QTO - Mission Pack 2 - Dissolution of Eternity 
-echo.	8) Play QTO - Mission Pack 3 - Abyss of Pandemonium 
-echo.
-echo.	--------------------------------------------------------------
-echo.	9) Exit
+echo.	5) Exit
 echo.	--------------------------------------------------------------
 echo.
-echo.   Your choice? [1-9]: 
-choice /c123456789 /s /N 	
-if errorlevel 9 goto :exit
-if errorlevel 8 goto :playlinux8
-if errorlevel 7 goto :playlinux7
-if errorlevel 6 goto :playlinux6
+echo.   Your choice? [1-5]: 
+choice /c12345 /s /N 	
 if errorlevel 5 goto :playlinux5
 if errorlevel 4 goto :playwin4
 if errorlevel 3 goto :playwin3
@@ -59,27 +48,6 @@ goto :launcher
 cd c:\Epsilon
 start darkplaces.exe -rogue -game abyss
 goto :launcher
-
-:playlinux5
-cd c:\Epsilon
-start /unix ./darkplaces-linux-x86_64-sdl
-goto :launcher
-
-:playlinux6
-cd c:\Epsilon
-start /unix ./darkplaces-linux-x86_64-sdl -hipnotic
-goto :launcher
-
-:playlinux7
-cd c:\Epsilon
-start /unix ./darkplaces-linux-x86_64-sdl -rogue
-goto :launcher
-
-:playlinux8
-cd c:\Epsilon
-start /unix ./darkplaces-linux-x86_64-sdl -rogue -game abyss
-goto :launcher
-
 
 :exit
 exit
