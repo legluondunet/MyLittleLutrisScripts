a7968523a7f456a5b51ed65af607a72839a580fd
legluondunet <legluondunet@gmail.com>
2021-10-15 23:39:50 +0200

    Add files via upload

--- Unreal/clean.sh
+++ Unreal/clean.sh
@@ -6,7 +6,9 @@ height=$3
 
 echo $lang $width $height $os
 
-if [ -d "drive_c/GOG Games/Unreal Gold" ]; then
+if [ -d "drive_c/Unreal Gold" ]; then
+mkdir "drive_c/GOG Games"
+mv "drive_c/Unreal Gold" "drive_c/GOG Games"
 cd "drive_c/GOG Games/Unreal Gold"
 fi
 
@@ -14,7 +16,8 @@ cd System
 
 cp Default.ini Default.ini.ori
 sed -i 's/GameRenderDevice=.*/GameRenderDevice=OpenGLDrv.OpenGLRenderDevice/' Default.ini
-sed -i 's/AudioDevice=.*/AudioDevice=FMODAudioDrv.FMODAudioDevice/' Default.ini
+#sed -i 's/GameRenderDevice=.*/GameRenderDevice=D3D9Drv.D3D9RenderDevice/' Default.ini
+sed -i 's/AudioDevice=.*/AudioDevice=ALAudio.ALAudioSubsystem/' Default.ini
 sed -i 's/Language=.*/Language='$lang'/' Default.ini
 sed -i 's/FirstRun=.*/FirstRun=227/' Default.ini
 sed -i 's/FullscreenViewportX=.*/FullscreenViewportX='$width'/' Default.ini
@@ -24,7 +27,8 @@ AudioDevice=ALAudio.ALAudioSubsystem
 
 cp Unreal.ini Unreal.ini.ori
 sed -i 's/GameRenderDevice=.*/GameRenderDevice=OpenGLDrv.OpenGLRenderDevice/' Unreal.ini
-sed -i 's/AudioDevice=.*/AudioDevice=FMODAudioDrv.FMODAudioDevice/' Unreal.ini
+#sed -i 's/GameRenderDevice=.*/GameRenderDevice=D3D9Drv.D3D9RenderDevice/' Unreal.ini
+sed -i 's/AudioDevice=.*/AudioDevice=ALAudio.ALAudioSubsystem/' Unreal.ini
 sed -i 's/Language=.*/Language='$lang'/' Unreal.ini
 sed -i 's/FirstRun=.*/FirstRun=227/' Unreal.ini
 sed -i 's/FullscreenViewportX=.*/FullscreenViewportX='$width'/' Unreal.ini
@@ -42,4 +46,6 @@ sed -i 's/FullscreenViewportX=.*/FullscreenViewportX='$width'/' UnrealLinux.ini
 sed -i 's/FullscreenViewportY=.*/FullscreenViewportY='$height'/' UnrealLinux.ini
 
 cd ..
-# rm -f -r gog* unins* Support.ico Launch* Gameux*
+
+rm -f -r app tmp GameuxInstallHelper.dll gfw_high.ico gog.ico gog_unreal_gold.sdb Support.ico
+
