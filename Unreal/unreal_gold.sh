739eb780d791d8eec8ef9dc34000f103b254e889
legluondunet <legluondunet@gmail.com>
2021-10-16 16:02:51 +0200

    Add files via upload

--- Unreal/unreal_gold.sh
+++ Unreal/unreal_gold.sh
@@ -1,7 +1,34 @@
 #!/bin/sh
+
 cd System
-MESA_EXTENSION_MAX_YEAR=1999
-#LD_LIBRARY_PATH=../lin_convenience_libs
-#export LD_LIBRARY_PATH
-#exec ./UnrealLinux.bin -log
-LD_LIBRARY_PATH=../libs ./UnrealLinux.bin -log
+
+
+zenity_base() {
+zenity "$@" --icon-name='lutris' --width="450" --height="250"  --title="Unreal Gold"
+}
+
+zenity_main_menu() { 
+play_ugold="Play Unreal Gold"
+play_pad="How to play Unreal Gold with a gamepad"
+
+CHOICE=$(zenity_base --list --radiolist  \
+        --column="" --column="" \
+        "TRUE" "$play_ugold" \
+        "FALSE" "$play_pad" )
+
+echo "valeur de la variable CHOICE: " $CHOICE
+    case "$CHOICE" in
+        "$play_ugold")
+            LD_LIBRARY_PATH=../libs ./UnrealLinux.bin -log
+            ;;
+        "$play_pad") 
+        xdg-open ../"gamepad profiles/unreal_gamepad_readme.html"
+
+zenity_main_menu
+	     ;;      
+    esac
+
+}
+
+zenity_main_menu
+	
