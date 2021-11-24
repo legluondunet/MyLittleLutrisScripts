182634ef70eaa9c685f215efe295a0077ac92579
legluondunet <legluondunet@gmail.com>
2021-11-24 20:51:28 +0100

    Delete Unreal_Gold.sh

--- Quake - The Offering/Unreal_Gold.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-cd System
-MESA_EXTENSION_MAX_YEAR=1999
-#LD_LIBRARY_PATH=../lin_convenience_libs
-#export LD_LIBRARY_PATH
-#exec ./UnrealLinux.bin -log
-LD_LIBRARY_PATH=../libs ./UnrealLinux.bin -log
