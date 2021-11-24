1e67a0d9151c786a0384e227921ca538c65895de
legluondunet <legluondunet@gmail.com>
2021-11-24 10:37:42 +0100

    Update Syberia.bat

--- Syberia/Syberia.bat
+++ Syberia/Syberia.bat
@@ -12,12 +12,12 @@ if %errorlevel% EQU 0 set result=0
 winedbg --command "info proc" | find Game.exe
 echo %errorlevel%
 if %errorlevel% EQU 0 set result=0
-#ping 127.0.0.1 -n 2 > nul
+rem ping 127.0.0.1 -n 2 > nul
 echo  %result%
 IF %result% NEQ 0 GOTO :search
 
 
-start nircmd.exe cmdwait 1000 win hide title Syberia
-start nircmd.exe cmdwait 1000 win hideshow title Syberia
+start nircmd.exe cmdwait 100 win hide title Syberia
+start nircmd.exe cmdwait 100 win hideshow title Syberia
 
 exit
