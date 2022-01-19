8a4cf8e1aa3a4fc9f292f846f97d18cd04d7228f
legluondunet <legluondunet@gmail.com>
2022-01-19 23:44:55 +0100

    Add files via upload

--- Sleeping Dogs/clean.sh
+++ Sleeping Dogs/clean.sh
@@ -6,7 +6,7 @@ cd drive_c
 mkdir "GOG Games"
 mv "Sleeping Dogs Definitive Edition" "GOG Games"; 
 cd "GOG Games/Sleeping Dogs Definitive Edition"
-cp app/* .
+cp -r app/* .
 
 rm -f -r app Callstacks commonappdata __redist tmp webcache.zip
 
@@ -28,7 +28,7 @@ fi
 
 
 echo $lang1 $lang2
-sed -e "s/English/"$lang1"/" -e "s/en-US/"$lang2"/" goggame-1741843789.info
+sed -i -e "s/English/"$lang1"/" -e "s/en-US/"$lang2"/" goggame-1741843789.info
 
 
 
