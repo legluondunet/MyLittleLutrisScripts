5c6f4382caaa4aa85f2b9ed03882a50ce671d788
legluondunet <legluondunet@gmail.com>
2022-01-19 23:12:09 +0100

    Add files via upload

---
+++ Sleeping Dogs/clean.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+lang1=$1
+
+cd drive_c
+mkdir "GOG Games"
+mv "Sleeping Dogs Definitive Edition" "GOG Games"; 
+cd "GOG Games/Sleeping Dogs Definitive Edition"
+cp app/* .
+
+rm -f -r app Callstacks commonappdata __redist tmp webcache.zip
+
+if [ $lang1 = "French" ]; then
+lang2="fr-FR"
+elif [ $lang1 = "Italian" ]; then
+lang2="it-IT"
+elif [ $lang1 = "German" ]; then
+lang2="ge-GE"
+elif [ $lang1 = "Spanish" ]; then
+lang2="sp-SP"
+elif [ $lang1 = "Polish" ]; then
+lang2="po-PO"
+elif [ $lang1 = "Russian" ]; then
+lang2="ru-RU"
+elif [ $lang1 = "English" ]; then
+exit
+fi
+
+
+echo $lang1 $lang2
+sed -e "s/English/"$lang1"/" -e "s/en-US/"$lang2"/" goggame-1741843789.info
+
+
+
+
