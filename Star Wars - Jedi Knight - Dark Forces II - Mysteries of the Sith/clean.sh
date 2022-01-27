cc3614ea287a9c5a2b4de3733183d1036338b5e2
legluondunet <legluondunet@gmail.com>
2022-01-27 07:54:10 +0100

    Add files via upload

--- Star Wars - Jedi Knight - Dark Forces II - Mysteries of the Sith/clean.sh
+++ Star Wars - Jedi Knight - Dark Forces II - Mysteries of the Sith/clean.sh
@@ -3,14 +3,11 @@
 rwidth=$1
 rheight=$2
 
-echo rwidth est égal à $rwidth
-echo rheight est égal à $rheight
+echo la résoluton est $rwidth x $rheight
 
-sed -i -e 's/ResolutionX="0"/ResolutionX="'$rwidth'"/g' -e 's/ResolutionY="0"/ResolutionY="'$rheight'"/g' drive_c/tmp/GamerProfile.xml
-mydoc=$(grep -i personal user.reg | grep $USER)
-echo mydoc est égal à $mydoc
-mydocpath=$(echo ${mydoc##*\\} | cut -d '"' -f 1)
-echo mydocpath est égal à $mydocpath
-mkdir -p "drive_c/users/"$USER"/""$mydocpath""/My Games/Far Cry 3"
-cp "drive_c/tmp/GamerProfile.xml" "drive_c/users/"$USER"/""$mydocpath""/My Games/Far Cry 3/"
-
+cd "drive_c/GOG Games"
+mv app "Star Wars Jedi Knight - Mysteries of the Sith"
+cd "Star Wars Jedi Knight - Mysteries of the Sith"
+cp __support/add/winmm_w10.dll winmm.dll
+rm -f -r ../tmp __support goggame-1422286819.* webcache.zip
+sed -i 's/"resolution".*/"resolution": [ '$rwidth', '$rheight' ],/' jkgm.json
