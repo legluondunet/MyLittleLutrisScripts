7fc2a06a8659fc7ea51046f2bba19d2fb6b2680f
Rathmox <55508107+Rathmox@users.noreply.github.com>
2022-07-09 03:12:59 +0200

    Delete OBSOLETE NO MORE MAINTAINED/Honkai Impact 3rd/gi_patch directory

--- OBSOLETE NO MORE MAINTAINED/Honkai Impact 3rd/gi_patch/ex_rpatch.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-current_path=$(pwd)
-cd "$current_path"
-cd "../Program Files/Genshin Impact/Genshin Impact game/"
-bash ../../../gi_patch/patch_revert.sh
-
-echo "Press enter to close this window..."
-read a
