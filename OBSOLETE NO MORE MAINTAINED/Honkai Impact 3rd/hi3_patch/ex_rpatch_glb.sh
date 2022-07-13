6bf1d6605567b0af6a4dd02cf73303349e236173
Rathmox <55508107+Rathmox@users.noreply.github.com>
2022-07-13 15:26:29 +0200

    Update and rename ex_rpatch.sh to ex_rpatch_glb.sh

--- OBSOLETE NO MORE MAINTAINED/Honkai Impact 3rd/hi3_patch/ex_rpatch_glb.sh
+++ OBSOLETE NO MORE MAINTAINED/Honkai Impact 3rd/hi3_patch/ex_rpatch_glb.sh
@@ -2,7 +2,7 @@
 
 current_path=$(pwd)
 cd "$current_path"
-cd "../Program Files/Genshin Impact/Genshin Impact game/"
+cd "../Program Files/Honkai Impact 3rd glb/Games/"
 bash ../../../gi_patch/patch_revert.sh
 
 echo "Press enter to close this window..."
