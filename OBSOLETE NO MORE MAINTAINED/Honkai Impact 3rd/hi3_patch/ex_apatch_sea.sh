f8a1ecbe5f0296b0ed7bd94b90e4e837dbac5757
Rathmox <55508107+Rathmox@users.noreply.github.com>
2022-07-13 15:41:12 +0200

    Create ex_apatch_sea.sh

---
+++ OBSOLETE NO MORE MAINTAINED/Honkai Impact 3rd/hi3_patch/ex_apatch_sea.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+current_path=$(pwd)
+cd "$current_path"
+cd "../Program Files//Honkai Impact 3 sea/Games/"
+bash ../../../hi3_patch/patch.sh
+
+echo "Press enter to close this window..."
+read a
