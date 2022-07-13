57fcdc4671b0ca8ba37139bd3a25367f25c55f54
Rathmox <55508107+Rathmox@users.noreply.github.com>
2022-07-13 16:26:05 +0200

    Create region_select.sh

---
+++ OBSOLETE NO MORE MAINTAINED/Honkai Impact 3rd/region_select.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+if test -f "hi3_menu_glb.sh"; then
+    mv hi3_menu_glb.sh hi3_menu.sh
+fi
+if test -f "hi3_menu_sea.sh"; then
+    mv hi3_menu_sea.sh hi3_menu.sh
+fi
+if test -f "hi3_menu_tw.sh"; then
+    mv hi3_menu_tw.sh hi3_menu.sh
+fi
+if test -f "hi3_menu_kr.sh"; then
+    mv hi3_menu_kr.sh hi3_menu.sh
+fi
