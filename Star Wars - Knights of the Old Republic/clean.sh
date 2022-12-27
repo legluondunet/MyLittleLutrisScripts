369e9bdfc80ffacc43d076d6429317e0a89756b5
legluondunet <legluondunet@gmail.com>
2022-12-27 23:43:43 +0100

    Add files via upload

--- Star Wars - Knights of the Old Republic/clean.sh
+++ Star Wars - Knights of the Old Republic/clean.sh
@@ -9,13 +9,14 @@ echo la résolution native du bureau est $rwidth $rheight
 #downloads mods
 mkdir drive_c/tmp
 cd drive_c/tmp
-wget https://github.com/legluondunet/MyLittleLutrisScripts/raw/master/Star%20Wars%20-%20Knights%20of%20the%20Old%20Republic/mods/k1hrm-1.4.7z
+wget https://github.com/legluondunet/MyLittleLutrisScripts/raw/master/Star%20Wars%20-%20Knights%20of%20the%20Old%20Republic/mods/k1hrm-1.5.7z
 wget https://github.com/legluondunet/MyLittleLutrisScripts/raw/master/Star%20Wars%20-%20Knights%20of%20the%20Old%20Republic/mods/%5BK1%5D_Main_Menu_Widescreen_Fix_v1.2.7z
 wget https://github.com/legluondunet/MyLittleLutrisScripts/raw/master/Star%20Wars%20-%20Knights%20of%20the%20Old%20Republic/mods/HD%20MENU%20AND%20UI%20Art%20v%201.0.7z.001
 wget https://github.com/legluondunet/MyLittleLutrisScripts/raw/master/Star%20Wars%20-%20Knights%20of%20the%20Old%20Republic/mods/HD%20MENU%20AND%20UI%20Art%20v%201.0.7z.002
 wget https://github.com/legluondunet/MyLittleLutrisScripts/raw/master/Star%20Wars%20-%20Knights%20of%20the%20Old%20Republic/mods/hd_pc_portraits-v1.0.7z
 wget https://github.com/legluondunet/MyLittleLutrisScripts/raw/master/Star%20Wars%20-%20Knights%20of%20the%20Old%20Republic/mods/hd_npc_portraits-v2.0.7z
 wget https://github.com/legluondunet/MyLittleLutrisScripts/raw/master/Star%20Wars%20-%20Knights%20of%20the%20Old%20Republic/mods/KoToR1IconMOD.7z
+wget https://github.com/legluondunet/MyLittleLutrisScripts/raw/master/Star%20Wars%20-%20Knights%20of%20the%20Old%20Republic/mods/Remastered%20UI%201.3-1499-1-3-1665238624.zip
 wget https://github.com/legluondunet/MyLittleLutrisScripts/raw/master/Star%20Wars%20-%20Knights%20of%20the%20Old%20Republic/mods/kotor_intro_cutscenes_1920x1080.7z
 
 # extract all 7z archives
@@ -29,6 +30,7 @@ cp "HD MENU AND UI Art v 1.0/"* "../GOG Games/Star Wars - KotOR/override"
 cp "hd_pc_portraits/Override/"* "../GOG Games/Star Wars - KotOR/override"
 cp "hd_npc_portraits/Override/"* "../GOG Games/Star Wars - KotOR/override"
 cp "KoToR1IconMOD/Override/"* "../GOG Games/Star Wars - KotOR/override"
+cp "Remastered UI 1.3/Override/"* "../GOG Games/Star Wars - KotOR/override"
 cp "movies_HD/1920x1080/"* "../GOG Games/Star Wars - KotOR/movies"
 
 # widescreen patch
