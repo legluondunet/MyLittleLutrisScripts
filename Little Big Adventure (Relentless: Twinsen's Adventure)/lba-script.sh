e34495fe64038d6a684c9523a478c19e64bb5a3f
legluondunet <legluondunet@gmail.com>
2023-02-08 23:07:29 +0100

    Add files via upload

---
+++ Little Big Adventure (Relentless: Twinsen's Adventure)/lba-script.sh
@@ -0,0 +1,7 @@
+rm -f -r *.DLL DOSBOX* SETUP.* *.INI LOADPATS.EXE LBA.D* LBA.CFG LBA.ico LBA1.conf DOS4GW.EXE LaunchLBA1.bat VOX
+cd tmp/Common/Music
+NUMBERS=$(ls *.mp3|sed -e 's/Track_//g' -e 's/\.mp3//g' -e 's/^0*//'|sort -n -r)
+for N in $NUMBERS; do NEW=$(($N - 1)); mv "Track_$N.mp3 -> Track_0$NEW.mp3"; done
+cd ..
+mv Vox Flac Music/* ../..
+
