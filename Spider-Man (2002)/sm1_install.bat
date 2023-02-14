ba9816ef97aeda9fd08027d915468a2353d8ae11
legluondunet <legluondunet@gmail.com>
2023-02-14 19:15:47 +0100

    Update sm1_install.bat

--- Spider-Man (2002)/sm1_install.bat
+++ Spider-Man (2002)/sm1_install.bat
@@ -1,10 +1,11 @@
-start "" setup.exe
+c:
+start "" c:\sm1\cd1\setup.exe
 
 :search
 winedbg --command "info wnd" | find "Insert CD" 1>NUL 2>NUL
 echo %errorlevel%
 if %errorlevel% EQU 0 goto :kill
-ping 127.0.0.1 -n 20 > nul
+ping 127.0.0.1 -n 5 > nul
 goto :search
 
 :kill
