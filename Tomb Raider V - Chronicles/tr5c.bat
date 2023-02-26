8a27e06bcf3f40e2daa93be837b38fd53d551ee4
legluondunet <legluondunet@gmail.com>
2023-02-26 17:51:09 +0100

    Update tr5c.bat

--- Tomb Raider V - Chronicles/tr5c.bat
+++ Tomb Raider V - Chronicles/tr5c.bat
@@ -15,20 +15,18 @@ echo.	------------------------------------------------
 echo.
 echo.	1) Tomb Raider V - Chronicles
 echo.	2) Setup dialog box
-echo.	3) Set ratio and FOV
-echo.	4) How to play TR5 with a gamepad
-echo.	5) Exit
+echo.	3) How to play TR5 with a gamepad
+echo.	4) Exit
 echo.
 echo.	------------------------------------------------
 echo.
 echo.
-echo.	Your choice [1-5]:
+echo.	Your choice [1-4]:
 
-choice /c12345 /s /N	
+choice /c1234 /s /N	
 
-if errorlevel 5 goto exit
-if errorlevel 4 goto gamepad
-if errorlevel 3 goto patch
+if errorlevel 4 goto exit
+if errorlevel 3 goto gamepad
 if errorlevel 2 goto setup
 if errorlevel 1 goto tr5
 
@@ -49,13 +47,7 @@ echo.	------------------------------------------------
 echo.
 echo.	Graphics Adapter	- choose ONLY dgVoodoo
 echo.	Output Settings 	- choose ONLY dgVoodoo
-echo.	Output Resolution	- for a native game experience
-echo.				prefer a resolution with 
-echo.				4:3 ratio and 32 bits colors
-echo.				if you are not sure choose 640x480x32
-echo.				- if you choose a resolution with a ratio 
-echo.				different from 4:3, you will need to patch
-echo.				the game to modify ratio and fov
+echo.	Output Resolution	- choose your native desktop resolution
 echo.	Texture Bit Depth	- choose 32RGBA for better texture quality
 echo.	Others options		- You should not need to change others options
 echo.
@@ -63,12 +55,6 @@ echo.	------------------------------------------------
 echo.
 echo.
 start pctomb5.exe -setup
-pause
-goto :launcher
-
-:patch
-cls
-start TombRaider234_Patch.exe
 pause
 goto :launcher
 
