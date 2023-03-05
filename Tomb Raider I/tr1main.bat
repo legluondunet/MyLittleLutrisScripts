1685698c32581ab413df3f54af3b1a565abf3288
legluondunet <legluondunet@gmail.com>
2023-03-05 10:11:24 +0100

    Update tr1main.bat

--- Tomb Raider I/tr1main.bat
+++ Tomb Raider I/tr1main.bat
@@ -1,22 +1,23 @@
 @echo off
 color 0E
-goto launcher
+
 c:
+cd c:\tomb1main
+
+goto :launcher
 
 :launcher
 cls
 echo.
 echo.
 echo. ------------------------------------------------------
-echo.	TOMB RAIDER I GOG Edition + TOMB1Main
+echo.		TOMB RAIDER I + TOMB1Main
 echo. ------------------------------------------------------
 echo.
 echo.	1) Play Tomb Raider I
 echo.	2) Play Tomb Raider I Unfinished Business
 echo.	3) Exit
 echo.
-echo. ------------------------------------------------------
-echo.	script by Le Gluon du Net   
 echo. ------------------------------------------------------
 echo.
 echo.
@@ -28,18 +29,13 @@ if errorlevel 1 goto tr1
 
 :tr1
 cls
-cd c:\tomb1main
-rem copy /Y TITLEHTR1.PCX TITLEH.PCX
-rem cd c:\TOMBATI\
 start tomb1main.exe
-goto launcher
+goto :launcher
 
 :tombub
 cls
-cd c:\tomb1main
-rem copy /Y TITLEHTR1UB.PCX TITLEH.PCX
 start tomb1main.exe -gold
-goto launcher
+goto :launcher
 
 :exit
 exit
