ce413489d24fd4769297c4054e7ed56362242e97
legluondunet <legluondunet@gmail.com>
2023-03-05 14:46:09 +0100

    Update tr3tla.bat

--- Tomb Raider III - The Lost Artefact/tr3tla.bat
+++ Tomb Raider III - The Lost Artefact/tr3tla.bat
@@ -42,51 +42,9 @@ echo.
 echo.
 echo.	------------------------------------------------
 echo.	TOMB RAIDER III - The Lost Artefact - Setup
-@echo off
-color 1F
-
-c:
-cd "c:\TR3TLA"
-
-goto :launcher
-
-:launcher
-cls
-echo.
-echo.
-echo.	------------------------------------------------
-echo.	TOMB RAIDER III - The Lost Artefact
 echo.	------------------------------------------------
 echo.
-echo.	1) Tomb Raider III - The Lost Artefact
-echo.	2) Setup dialog box
-echo.	3) How to play TR3TLA with a gamepad
-echo.	4) Exit
-echo.
-echo.	------------------------------------------------
-echo.
-echo.
-
-
-choice /c1234 /s /N Your choice? [1-4]:
-if errorlevel 4 goto exit
-if errorlevel 3 goto gamepad
-if errorlevel 2 goto setup
-if errorlevel 1 goto tr3tla
-
-
-:tr3tla
-cls
-start tomb3.exe -gold
-goto :launcher
-
-:setup
-cls
-echo.
-echo.
-echo.	------------------------------------------------
-echo.	TOMB RAIDER III - The Lost Artefact - Setup
-echo.	------------------------------------------------
+echo.	Setup:
 echo.
 echo.	Graphics Adapter	- choose ONLY dgVoodoo
 echo.	Output Settings 	- choose ONLY dgVoodoo
@@ -106,22 +64,6 @@ cls
 start explorer c:\TR3TLA\gamepad profiles\
 start iexplore c:\TR3TLA\gamepad profiles\tr3_gamepad_readme.html
 pause
-goto :launcher
-
-
-:exit
-exit
-echo.
-echo.
-start tomb3.exe -setup
-pause
-goto :launcher
-
-
-:gamepad
-cls
-start explorer c:\TR3TLA\gamepad profiles\
-start iexplore c:\TR3TLA\gamepad profiles\tr3_gamepad_readme.html
 goto :launcher
 
 
