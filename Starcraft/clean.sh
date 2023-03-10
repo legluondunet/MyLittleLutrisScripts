3d682b0f2e59490f69be36f479ce5723eed47cf7
legluondunet <legluondunet@gmail.com>
2023-03-10 03:18:58 +0100

    Update clean.sh

--- Starcraft/clean.sh
+++ Starcraft/clean.sh
@@ -12,6 +12,5 @@ cd "drive_c/Program Files (x86)"
 ln -s "../Program Files/Starcraft" "Starcraft"
 echo 'ln -s "../Program Files/Starcraft" "Starcraft"'
 fi
-cd 
-sed -i -e 's/fullscreen=.*/fullscreen=true/' -e 's/windowed=.*/windowed=true/' -e 's/maintas=.*/maintas=true/' -e 's/adjmouse=.*/adjmouse=true/' "drive-c//Program Files/Starcraft/ddraw.ini"
-find "$cdpath/"* -iname install.exe | xargs -I {} cp {} starcraft.mpq
+sed -i -e 's/fullscreen=.*/fullscreen=true/' -e 's/windowed=.*/windowed=true/' -e 's/maintas=.*/maintas=true/' -e 's/adjmouse=.*/adjmouse=true/' ddraw.ini
+find $cdpath/* -iname install.exe | xargs -I {} cp {} starcraft.mpq
