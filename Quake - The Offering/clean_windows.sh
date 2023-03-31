b590f505638b761f7a72c6d0404563e59943438a
legluondunet <legluondunet@gmail.com>
2023-04-01 00:20:09 +0200

    Update clean_windows.sh

--- Quake - The Offering/clean_windows.sh
+++ Quake - The Offering/clean_windows.sh
@@ -9,12 +9,12 @@ height=$2
 
 cd drive_c/Epsilon/id1
 mv PAK0.PAK PAK0.PAK.DEMO
-cp ../../tmp/app/id1/PAK* .
+cp ../../tmp/id1/PAK* .
 mkdir -p sound/cdtracks
 
-cd ../../tmp/app/
+cd ../../tmp
 ./bchunk game.gog game.cue track0 -w
-mv *.wav ../../Epsilon/id1/sound/cdtracks
+mv *.wav ../Epsilon/id1/sound/cdtracks
 
 ##################
 # Mission pack 1 #
@@ -24,8 +24,8 @@ mv *.wav ../../Epsilon/id1/sound/cdtracks
 ./7z x track001.iso -ogamea
 mkdir -p gamea/HIPNOTIC/sound/cdtracks
 mv *.wav gamea/HIPNOTIC/sound/cdtracks
-mv gamea/HIPNOTIC/* ../../Epsilon/hipnotic/
-mv ../../Epsilon/hipnotic/CONFIG.CFG ../../Epsilon/hipnotic/CONFIG.BAK
+mv gamea/HIPNOTIC/* ../Epsilon/hipnotic/
+mv ../Epsilon/hipnotic/CONFIG.CFG ../Epsilon/hipnotic/CONFIG.BAK
 
 
 ##################
@@ -36,9 +36,9 @@ mv ../../Epsilon/hipnotic/CONFIG.CFG ../../Epsilon/hipnotic/CONFIG.BAK
 ./7z x track001.iso -ogamed
 mkdir -p gamed/ROGUE/sound/cdtracks
 mv *.wav gamed/ROGUE/sound/cdtracks
-mv gamed/ROGUE/* ../../Epsilon/rogue/
+mv gamed/ROGUE/* ../Epsilon/rogue/
 
-cd ../../Epsilon
+cd ../Epsilon
 sed -i -e "/vid_width/d" -e "/vid_height/d" -e '/vid_vsync/d' -e '/crosshair/d' id1/config.cfg
 sed -i -e '$a \"vid_width" "'$width'"' -e '$a \"vid_height" "'$height'"' -e '$a \"vid_vsync" "1"' -e '$a \"crosshair" "1"' id1/config.cfg
 
@@ -51,4 +51,3 @@ if [ -d tmp ]
 then
 rm -f -r tmp
 fi
-
