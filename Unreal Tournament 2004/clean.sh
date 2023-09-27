19e2d8ea9ac29cd02c13a122f18fbb2d917f8afd
legluondunet <legluondunet@gmail.com>
2023-09-27 23:09:51 +0200

    Update clean.sh

--- Unreal Tournament 2004/clean.sh
+++ Unreal Tournament 2004/clean.sh
@@ -7,26 +7,23 @@ os=$3
 if [ $os = "Windows" ]
 then
 echo Windows
-cd "drive_c/GOG Games/Unreal Tournament 2004"
+cd "drive_c/GOG Games/Unreal Tournament 2004/System/"
 elif [ $os = "Linux" ]
 then
 echo Linux
+cd system
+echo "Create cdkey file for Linux version"
+echo 3QHVC-DHA9K-A84RE-KYRT4 > cdkey
 fi
 
-cd System
-
 echo "la résolution actuelle est $width x $height"
-echo "modification de la configuration"
-
 sed -i "s/RenderDevice=.*/RenderDevice=OpenGLDrv.OpenGLRenderDevice/g" Default.ini
 sed -i "s/FullscreenViewportX.*/FullscreenViewportX=$width/g" Default.ini
 sed -i "s/FullscreenViewportY.*/FullscreenViewportY=$height/g" Default.ini
-sed -i "s/UseVSync=False/UseVSync=True/g" Default.ini
-sed -i "s/UseDefaultDriver=True/UseDefaultDriver=False/g" Default.ini
-
-echo "Create cdkey file for Linux version"
-
-echo 3QHVC-DHA9K-A84RE-KYRT4 > cdkey
+sed -i "s/WindowedViewportX.*/WindowedViewportX=$width/g" Default.ini
+sed -i "s/WindowedViewportY.*/WindowedViewportY=$height/g" Default.ini
+#sed -i "s/UseVSync=False/UseVSync=True/g" Default.ini
+#sed -i "s/UseDefaultDriver=True/UseDefaultDriver=False/g" Default.ini
 
 cd ..
 
