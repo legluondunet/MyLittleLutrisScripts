15556a520b1c953f6c3ca05a1a617a625ed6a3d3
legluondunet <legluondunet@gmail.com>
2023-11-09 18:59:47 +0100

    Update clean.sh

--- Starcraft/clean.sh
+++ Starcraft/clean.sh
@@ -8,14 +8,18 @@ then
 cd "drive_c/Program Files"
 ln -s "../Program Files (x86)/Starcraft" "Starcraft"
 echo 'ln -s "../Program Files (x86)/Starcraft" "Starcraft"'
-else
+elif [ ! -d "drive_c/Program Files (x86)/Starcraft" ]
+then
 cd "drive_c/Program Files (x86)"
 ln -s "../Program Files/Starcraft" "Starcraft"
 echo 'ln -s "../Program Files/Starcraft" "Starcraft"'
 fi
 
+cd starcraft
+
 if [ $cdpath ]
 then
 find $cdpath/* -iname install.exe | xargs -I {} cp {} starcraft.mpq
 echo $cdpath'/* -iname install.exe | xargs -I {} cp {} starcraft.mpq'
+chmod +rw StarCraft.mpq;
 fi
