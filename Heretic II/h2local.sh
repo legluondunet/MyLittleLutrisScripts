ba6a73d95135794d9916ca132c211538dfc6ec7f
legluondunet <legluondunet@gmail.com>
2024-01-16 17:48:11 +0100

    Update h2local.sh

--- Heretic II/h2local.sh
+++ Heretic II/h2local.sh
@@ -18,7 +18,7 @@ fi
 
 rm -f -r french-1.pak german-1.pak italian-1.pak spanish-1.pak htic2-1.pak gamemsg.txt levelmsg.txt menus.cfg
 
-wget "https://github.com/legluondunet/MyLittleLutrisScripts/raw/master/Heretic%20II/lang/"$lang"-1.pak.tar.xz"
+wget --no-check-certificate "https://github.com/legluondunet/MyLittleLutrisScripts/raw/master/Heretic%20II/lang/"$lang"-1.pak.tar.xz"
 tar xfv $lang-1.pak.tar.xz
 mv $lang-1.pak htic2-1.pak
 rm -f -r $lang-1.pak.tar.xz
