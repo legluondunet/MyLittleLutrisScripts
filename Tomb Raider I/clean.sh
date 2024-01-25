f472ea344e3e6c5cc6ef2c8c2ca33d3f891409ec
legluondunet <legluondunet@gmail.com>
2024-01-25 21:10:18 +0100

    Update clean.sh

--- Tomb Raider I/clean.sh
+++ Tomb Raider I/clean.sh
@@ -7,7 +7,7 @@ cd "drive_c/TOMBATI/"
 sed -i -e "s/localization = false/localization = true/" -e "s/localization_locale =.*/localization_locale = $lang/" patches/tomb*
 if [[ "$lang" =~ ^(en_gb|fr_fr|de_de|jp_jp)$ ]] 
 then
-wget "https://github.com/legluondunet/MyLittleLutrisScripts/raw/master/Tomb%20Raider%20I/tombati_multi_lang/tombati_audio_$lang.zip"
+wget --no-check-certificate "https://github.com/legluondunet/MyLittleLutrisScripts/raw/master/Tomb%20Raider%20I/tombati_multi_lang/tombati_audio_$lang.zip"
 ls
 7z e -y tombati_audio_$lang.zip -omusic/
 fi
