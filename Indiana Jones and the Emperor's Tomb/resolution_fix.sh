a32276ace1988d039b0e48f059ea6b54dd6c9adf
legluondunet <legluondunet@gmail.com>
2024-02-11 19:50:09 +0100

    Update resolution_fix.sh

--- Indiana Jones and the Emperor's Tomb/resolution_fix.sh
+++ Indiana Jones and the Emperor's Tomb/resolution_fix.sh
@@ -1,13 +1,4 @@
 #!/bin/bash
 
 cd "drive_c/GOG Games/Indiana Jones and the Emperor's Tomb/GameData/indy"
-{
-	echo 'fpsLimit=60'
-	echo 'notext=1'
-	echo 'pack=0'
-	echo 'title=0'
-	echo 'res_width=1280'
-	echo 'res_height=960'
-	echo 'res_bpp=32'
-	echo 'subtitles=true'
-} > vars.cfg
+cp vars.cfg.default vars.cfg
