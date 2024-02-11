451b852b06e725a9f88e920723180bcb69310141
legluondunet <legluondunet@gmail.com>
2024-02-11 18:09:06 +0100

    Add files via upload

---
+++ Indiana Jones and the Emperor's Tomb/resolution.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+cd "drive_c/GOG Games/Indiana Jones and the Emperor's Tomb/GameData/indy"
+
+{
+	echo 'fpsLimit=30'
+	echo 'notext=1'
+	echo 'pack=0'
+	echo 'title=0
+	echo 'res_width=1280'
+	echo 'res_height=960'
+	echo 'res_bpp=32'
+	echo 'subtitles=true'
+
+} >> vars.cfg
