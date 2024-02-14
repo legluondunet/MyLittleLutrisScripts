9a86ad42268e0b7014646ab100ddfa1d5666e6a9
legluondunet <legluondunet@gmail.com>
2024-02-15 00:37:43 +0100

    Update clean-openlara.sh

--- Tomb Raider II/clean-openlara.sh
+++ Tomb Raider II/clean-openlara.sh
@@ -10,6 +10,8 @@ mv TITLE.tr2 TITLE.TR2
 cp TITLE.PNG TITLEUS.PNG
 cp MAIN.SFX ../../audio/2
 
+if [ "$1" == "GOG" ]
+then
 cd ../../audio/2
 echo $LD_LIBRARY_PATH
 export LD_LIBRARY_PATH=""
@@ -19,6 +21,30 @@ export LD_LIBRARY_PATH=""
 	mv $file $newfile
 	done
 cd ../../video/2
+	for file in *
+	do mv -- "$file" "${file^^}"
+	done
+fi
 
-for file in *; do mv -- "$file" "${file^^}"; done
-
+if [ "$1" == "STEAM" ]
+then
+cd ../../audio/2
+echo $LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=""
+	for file in *.mp3
+	do
+	filename="${file%.*}"
+	n=${#filename}
+	echo traitement du fichier $file
+	echo $filename est composé de $n caractères
+		if [ "$n" == "1" ]
+		then 
+		filename="0$filename"
+		echo le nom de fichier $filename comporte à présent 2 caractères
+		fi
+	echo $file
+	echo exécution de la commande "ffmpeg -i $file track_$filename.ogg"
+	ffmpeg -i $file track_$filename.ogg
+	done
+rm -f -r *.mp3
+fi
