a75652b2a5eb4b0af8bed164c6254af0dad81696
legluondunet <legluondunet@gmail.com>
2024-03-12 21:09:41 +0100

    Update settings.bat

--- L.A.Noire/settings.bat
+++ L.A.Noire/settings.bat
@@ -4,9 +4,11 @@ cd "%USERPROFILE%\Documents\Rockstar Games\L.A. Noire"
 dir
 
 if exist "c:\Program Files (x86)\Steam\steamapps\common\L.A.Noire" (
+echo le jeu est installé
 attrib -r settings.ini
 settings.ini
 attrib +r settings.ini
 ) else (
+echo le jeu n'est pas installé
 attrib +r settings.ini
 )
