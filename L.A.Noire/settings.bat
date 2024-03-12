9d7b870592f50fcfaefb0fd0cc5f1bcea29885d2
legluondunet <legluondunet@gmail.com>
2024-03-12 20:21:15 +0100

    Create settings.bat

---
+++ L.A.Noire/settings.bat
@@ -0,0 +1,12 @@
+@echo on
+c:
+cd "%USERPROFILE%\Documents\Rockstar Games\L.A. Noire"
+dir
+
+if exist "c:\Program Files (x86)\Steam\steamapps\common\L.A.Noire" (
+attrib -r settings.ini
+settings.ini
+attrib +r settings.ini
+) else (
+attrib +r settings.ini
+)
