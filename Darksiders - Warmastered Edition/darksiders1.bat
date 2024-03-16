b01d5b104d65a1d755ee68962559c503e169cb2a
legluondunet <legluondunet@gmail.com>
2024-03-17 00:34:06 +0100

    Update darksiders1.bat

--- Darksiders - Warmastered Edition/darksiders1.bat
+++ Darksiders - Warmastered Edition/darksiders1.bat
@@ -6,8 +6,6 @@ reg add "HKLM\SOftware\Classes\\CLSID\\{82D353DF-90BD-4382-8BC2-3F6192B76E34}\\I
 
 reg add "HKLM\Software\\Classes\\CLSID\\{2EEB4ADF-4578-4D10-BCA7-BB955F56320A}\\InprocServer32" /ve /d "C:\\windows\\System32\\WMADMOD.DLL" /f
 
-del /q windows\system32\mfplat.dll
-
 if exist "c:\Program Files (x86)\Steam" (
 echo Version Steam
 cd "Program Files (x86)\Steam"
