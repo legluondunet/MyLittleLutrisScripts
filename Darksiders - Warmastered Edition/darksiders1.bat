8c99f055fb31d83f9198c8c646fcbcb17ad173e7
legluondunet <legluondunet@gmail.com>
2024-03-16 22:39:50 +0100

    Update darksiders1.bat

--- Darksiders - Warmastered Edition/darksiders1.bat
+++ Darksiders - Warmastered Edition/darksiders1.bat
@@ -1,4 +1,4 @@
-@echo off
+@echo on
 
 c:
 
@@ -6,6 +6,18 @@ reg add "HKLM\SOftware\Classes\\CLSID\\{82D353DF-90BD-4382-8BC2-3F6192B76E34}\\I
 
 reg add "HKLM\Software\\Classes\\CLSID\\{2EEB4ADF-4578-4D10-BCA7-BB955F56320A}\\InprocServer32" /ve /d "C:\\windows\\System32\\WMADMOD.DLL" /f
 
+del /q windows\system32\mfplat.dll
+
+if exist "c:\Program Files (x86)\Steam" (
+echo Version Steam
+cd "Program Files (x86)\Steam"
+start steam.exe
+)
+
+if exist "GOG Games" (
+echo Version GOG
+cd "GOG Games\Darksiders Warmastered Edition"
 start darksiders1.exe
+)
 
 exit
