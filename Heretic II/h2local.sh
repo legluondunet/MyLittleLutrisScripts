c783fb964be3f8cc43bdfca0304c8a317c533859
legluondunet <legluondunet@gmail.com>
2025-12-19 09:54:25 +0100

    Update h2local.sh

--- Heretic II/h2local.sh
+++ Heretic II/h2local.sh
@@ -3,21 +3,15 @@
 lang=$1
 echo "Language choisi: " $lang
 
-if [ $lang = "english" ]; then exit
-fi
-
-if [ -f "drive_c/Program Files (x86)/Heretic II/Heretic2.exe" ]; then
-echo "installation CD Windows localisée"
-cd "drive_c/Program Files (x86)/Heretic II/Base"
-else echo "Le jeu n'a pas été trouvé"
+if [ $lang = "english" ] || [ ! -f "drive_c/Program Files (x86)/Heretic II/Heretic2.exe" ]; then echo "aucune action"
 exit
-fi
-
+else
+cd "drive_c/Program Files (x86)/Heretic II/Base"
 rm -f -r french-1.pak german-1.pak italian-1.pak spanish-1.pak htic2-1.pak gamemsg.txt levelmsg.txt menus.cfg
-
 wget --no-check-certificate "https://github.com/legluondunet/MyLittleLutrisScripts/raw/master/Heretic%20II/lang/"$lang"-1.pak.tar.xz"
+
 tar xfv $lang-1.pak.tar.xz
 mv $lang-1.pak htic2-1.pak
 rm -f -r $lang-1.pak.tar.xz
 
-
+fi
