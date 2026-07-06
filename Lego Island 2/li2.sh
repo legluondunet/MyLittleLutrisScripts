3d2a2860d402d6e672d7693f457a250fd3720651
legluondunet <legluondunet@gmail.com>
2026-07-06 18:15:50 +0200

    Update li2.sh

--- Lego Island 2/li2.sh
+++ Lego Island 2/li2.sh
@@ -17,12 +17,4 @@ while true; do
 
     # pause
     sleep 5
-done#!/bin/bash
-
-while ! xdotool search --name "LEGO Company" >/dev/null 2>&1; do
-    sleep 3
 done
-
-xdotool search --name "LEGO Company" windowactivate
-sleep 1
-xdotool search --name "LEGO Company" windowactivate
